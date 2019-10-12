bc0f2301f59e5f50c93d63b785be390b9e5a0676
joshprzybyszewski <josh.prezby@gmail.com>
2019-10-12 17:29:56 -0500

    fix broken tests
    these should be this way

--- cards/card_test.go
+++ cards/card_test.go
@@ -12,29 +12,53 @@ func TestNewCardFromString(t *testing.T) {
 		input   string
 		expCard Card
 	}{{
-		desc:    `ace of hearts`,
-		input:   `AH`,
-		expCard: Card{Suit: Hearts, Value: 1},
+		desc:  `ace of hearts`,
+		input: `AH`,
+		expCard: Card{
+			Suit:      Hearts,
+			Value:     1,
+			deckValue: 39,
+		},
 	}, {
-		desc:    `two of diamonds`,
-		input:   `2D`,
-		expCard: Card{Suit: Diamonds, Value: 2},
+		desc:  `two of diamonds`,
+		input: `2D`,
+		expCard: Card{
+			Suit:      Diamonds,
+			Value:     2,
+			deckValue: 27,
+		},
 	}, {
-		desc:    `10 of spades`,
-		input:   `10S`,
-		expCard: Card{Suit: Spades, Value: 10},
+		desc:  `10 of spades`,
+		input: `10S`,
+		expCard: Card{
+			Suit:      Spades,
+			Value:     10,
+			deckValue: 9,
+		},
 	}, {
-		desc:    `Jack of Clubs`,
-		input:   `JC`,
-		expCard: Card{Suit: Clubs, Value: 11},
+		desc:  `Jack of Clubs`,
+		input: `JC`,
+		expCard: Card{
+			Suit:      Clubs,
+			Value:     11,
+			deckValue: 23,
+		},
 	}, {
-		desc:    `Queen of Hearts`,
-		input:   `QH`,
-		expCard: Card{Suit: Hearts, Value: 12},
+		desc:  `Queen of Hearts`,
+		input: `QH`,
+		expCard: Card{
+			Suit:      Hearts,
+			Value:     12,
+			deckValue: 50,
+		},
 	}, {
-		desc:    `King of Diamonds`,
-		input:   `KD`,
-		expCard: Card{Suit: Diamonds, Value: 13},
+		desc:  `King of Diamonds`,
+		input: `KD`,
+		expCard: Card{
+			Suit:      Diamonds,
+			Value:     13,
+			deckValue: 38,
+		},
 	}}
 
 	for _, tc := range testCases {
@@ -48,29 +72,53 @@ func TestNewCardFromStringWithWeirdInput(t *testing.T) {
 		input   string
 		expCard Card
 	}{{
-		desc:    `ace of hearts`,
-		input:   `A♡`,
-		expCard: Card{Suit: Hearts, Value: 1},
+		desc:  `ace of hearts`,
+		input: `A♡`,
+		expCard: Card{
+			Suit:      Hearts,
+			Value:     1,
+			deckValue: 39,
+		},
 	}, {
-		desc:    `two of diamonds`,
-		input:   `2♢`,
-		expCard: Card{Suit: Diamonds, Value: 2},
+		desc:  `two of diamonds`,
+		input: `2♢`,
+		expCard: Card{
+			Suit:      Diamonds,
+			Value:     2,
+			deckValue: 27,
+		},
 	}, {
-		desc:    `10 of spades`,
-		input:   `10s`,
-		expCard: Card{Suit: Spades, Value: 10},
+		desc:  `10 of spades`,
+		input: `10s`,
+		expCard: Card{
+			Suit:      Spades,
+			Value:     10,
+			deckValue: 9,
+		},
 	}, {
-		desc:    `Jack of Clubs`,
-		input:   `11c`,
-		expCard: Card{Suit: Clubs, Value: 11},
+		desc:  `Jack of Clubs`,
+		input: `11c`,
+		expCard: Card{
+			Suit:      Clubs,
+			Value:     11,
+			deckValue: 23,
+		},
 	}, {
-		desc:    `Queen of Hearts`,
-		input:   `12♥︎`,
-		expCard: Card{Suit: Hearts, Value: 12},
+		desc:  `Queen of Hearts`,
+		input: `12♥︎`,
+		expCard: Card{
+			Suit:      Hearts,
+			Value:     12,
+			deckValue: 50,
+		},
 	}, {
-		desc:    `King of Diamonds`,
-		input:   `13♦`,
-		expCard: Card{Suit: Diamonds, Value: 13},
+		desc:  `King of Diamonds`,
+		input: `13♦`,
+		expCard: Card{
+			Suit:      Diamonds,
+			Value:     13,
+			deckValue: 38,
+		},
 	}}
 
 	for _, tc := range testCases {
