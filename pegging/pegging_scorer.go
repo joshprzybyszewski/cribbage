6e2cf4a1d92de541ebb500add110274786fed884
cszczepaniak <cszczepaniak@gmail.com>
2019-10-12 15:49:38 -0500

    score runs during pegging

--- pegging/pegging_scorer.go
+++ pegging/pegging_scorer.go
@@ -2,15 +2,17 @@ package pegging
 
 import (
 	"errors"
+	"sort"
 
 	"github.com/joshprzybyszewski/cribbage/cards"
 )
 
 var (
 	errSameCardTwice = errors.New(`it's impossible to peg the same card twice`)
-	errTooManyCards = errors.New(`it's impossible to have this many cards pegged ever`)
+	errTooManyCards  = errors.New(`it's impossible to have this many cards pegged ever`)
 )
 
+// PointsForCard needs a real comment to make the linter happier
 func PointsForCard(prevCards []cards.Card, c cards.Card) (int, error) {
 	if err := validatePrevCards(prevCards, c); err != nil {
 		return 0, err
@@ -39,12 +41,29 @@ func PointsForCard(prevCards []cards.Card, c cards.Card) (int, error) {
 		}
 		points += 2 * (len(cardsToAnalyze) - i)
 	}
-
+	if points > 0 {
+		return points, nil
+	}
+	sortedCards := append(cardsToAnalyze, c)
+	sort.Slice(sortedCards, func(i, j int) bool {
+		return sortedCards[i].Value < sortedCards[j].Value
+	})
+	points = 1
+	for i := len(sortedCards) - 1; i >= 1; i-- {
+		if sortedCards[i].Value == sortedCards[i-1].Value+1 {
+			points++
+		} else {
+			break
+		}
+	}
+	if points == 1 {
+		return 0, nil
+	}
 	return points, nil
 }
 
 func validatePrevCards(prevCards []cards.Card, c cards.Card) error {
-	if len(prevCards) > 4 * 4 {
+	if len(prevCards) > 4*4 {
 		// 4 players can each peg four cards. that's our max
 		return errTooManyCards
 	}
