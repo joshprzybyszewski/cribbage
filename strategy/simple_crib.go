faf260e8c57acfd6daf9022d43a8e6c0e9f048a7
cszczepaniak <cszczepaniak@gmail.com>
2019-10-16 22:59:34 -0500

    pass the new tests

--- strategy/simple_crib.go
+++ strategy/simple_crib.go
@@ -1,25 +1,27 @@
 package strategy
 
 import (
-	"sort"
-
 	"github.com/joshprzybyszewski/cribbage/cards"
 )
 
-// AvoidCribFifteens gg golint
+// AvoidCribFifteens tries to return a set of cards which does not add up to 15
 func AvoidCribFifteens(desired int, hand []cards.Card) []cards.Card {
+	// Currently this function uses a very piecewise solution... but it passes the tests :)
 	cribCards := make([]cards.Card, 0, desired)
-	sort.Slice(hand, func(i, j int) bool {
-		return prioritize(hand[i], 5) < prioritize(hand[j], 5)
-	})
-	for i := 0; i < desired; i++ {
-		cribCards = append(cribCards, hand[i])
+	if desired == 1 {
+		cribCards = append(cribCards, hand[0])
+		return cribCards
 	}
-	// Sorting because the test is expecting the cards in a certain order...
-	sort.Slice(cribCards, func(i, j int) bool {
-		return cribCards[i].Value < cribCards[j].Value
-	})
-	return cribCards
+	for i := 0; i < len(hand)-1; i++ {
+		c1 := hand[i]
+		otherCards := hand[i+1:]
+		for _, c2 := range otherCards {
+			if c1.PegValue()+c2.PegValue() != 15 {
+				return append(cribCards, c1, c2)
+			}
+		}
+	}
+	return append(cribCards, hand[0], hand[1])
 }
 
 func GiveCribFifteens(desired int, hand []cards.Card) []cards.Card {
