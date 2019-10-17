e0db883cb8d4b459e88bf123e61e445f4b2965a0
cszczepaniak <cszczepaniak@gmail.com>
2019-10-16 23:22:07 -0500

    DRY and remove unused function

--- strategy/simple_crib.go
+++ strategy/simple_crib.go
@@ -6,73 +6,37 @@ import (
 
 // AvoidCribFifteens tries to return a set of cards which does not add up to 15
 func AvoidCribFifteens(desired int, hand []cards.Card) []cards.Card {
-	// Currently this function uses a very piecewise solution... but it passes the tests :)
-	cribCards := make([]cards.Card, 0, desired)
-	if desired == 1 {
-		cribCards = append(cribCards, hand[0])
-		return cribCards
+	objFunc := func(c1, c2 cards.Card) bool {
+		return c1.PegValue()+c2.PegValue() != 15
 	}
-	for i := 0; i < len(hand)-1; i++ {
-		c1 := hand[i]
-		otherCards := hand[i+1:]
-		for _, c2 := range otherCards {
-			if c1.PegValue()+c2.PegValue() != 15 {
-				return append(cribCards, c1, c2)
-			}
-		}
-	}
-	return append(cribCards, hand[0], hand[1])
+	return determineCribCards(desired, hand, objFunc)
 }
 
 // GiveCribFifteens tries to return a set of cards which adds up to 15
 func GiveCribFifteens(desired int, hand []cards.Card) []cards.Card {
-	// Currently this function uses a very piecewise solution... but it passes the tests :)
-	cribCards := make([]cards.Card, 0, desired)
-	if desired == 1 {
-		cribCards = append(cribCards, hand[0])
-		return cribCards
+	objFunc := func(c1, c2 cards.Card) bool {
+		return c1.PegValue()+c2.PegValue() == 15
 	}
-	for i := 0; i < len(hand)-1; i++ {
-		c1 := hand[i]
-		otherCards := hand[i+1:]
-		for _, c2 := range otherCards {
-			if c1.PegValue()+c2.PegValue() == 15 {
-				return append(cribCards, c1, c2)
-			}
-		}
-	}
-	return append(cribCards, hand[0], hand[1])
-}
-
-func prioritize(c cards.Card, value int) int {
-	if c.Value == value {
-		return 1
-	}
-	return 0
+	return determineCribCards(desired, hand, objFunc)
 }
 
 // AvoidCribPairs tries to return a set of cards which does not make a pair (unequal value)
 func AvoidCribPairs(desired int, hand []cards.Card) []cards.Card {
-	// Currently this function uses a very piecewise solution... but it passes the tests :)
-	cribCards := make([]cards.Card, 0, desired)
-	if desired == 1 {
-		cribCards = append(cribCards, hand[0])
-		return cribCards
+	objFunc := func(c1, c2 cards.Card) bool {
+		return c1.Value != c2.Value
 	}
-	for i := 0; i < len(hand)-1; i++ {
-		c1 := hand[i]
-		otherCards := hand[i+1:]
-		for _, c2 := range otherCards {
-			if c1.Value != c2.Value {
-				return append(cribCards, c1, c2)
-			}
-		}
-	}
-	return append(cribCards, hand[0], hand[1])
+	return determineCribCards(desired, hand, objFunc)
 }
 
 // GiveCribPairs tries to return a set of cards that makes a pair (equal value)
 func GiveCribPairs(desired int, hand []cards.Card) []cards.Card {
+	objFunc := func(c1, c2 cards.Card) bool {
+		return c1.Value == c2.Value
+	}
+	return determineCribCards(desired, hand, objFunc)
+}
+
+func determineCribCards(desired int, hand []cards.Card, objectiveFunc func(c1, c2 cards.Card) bool) []cards.Card {
 	// Currently this function uses a very piecewise solution... but it passes the tests :)
 	cribCards := make([]cards.Card, 0, desired)
 	if desired == 1 {
@@ -83,7 +47,7 @@ func GiveCribPairs(desired int, hand []cards.Card) []cards.Card {
 		c1 := hand[i]
 		otherCards := hand[i+1:]
 		for _, c2 := range otherCards {
-			if c1.Value == c2.Value {
+			if objectiveFunc(c1, c2) {
 				return append(cribCards, c1, c2)
 			}
 		}
