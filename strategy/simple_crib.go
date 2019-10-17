3d275291dce666b8a4ad4f7e47b4eb5b0cd123b7
cszczepaniak <cszczepaniak@gmail.com>
2019-10-16 22:09:57 -0500

    pass the first test case for AvoidCribFifteens

--- strategy/simple_crib.go
+++ strategy/simple_crib.go
@@ -1,15 +1,35 @@
 package strategy
 
 import (
+	"sort"
+
 	"github.com/joshprzybyszewski/cribbage/cards"
 )
 
+// AvoidCribFifteens gg golint
 func AvoidCribFifteens(desired int, hand []cards.Card) []cards.Card {
-
-	return nil
+	cribCards := make([]cards.Card, 0, desired)
+	sort.Slice(hand, func(i, j int) bool {
+		return prioritize(hand[i], 5) < prioritize(hand[j], 5)
+	})
+	for i := 0; i < desired; i++ {
+		cribCards = append(cribCards, hand[i])
+	}
+	// Sorting because the test is expecting the cards in a certain order...
+	sort.Slice(cribCards, func(i, j int) bool {
+		return cribCards[i].Value < cribCards[j].Value
+	})
+	return cribCards
 }
 
 func GiveCribFifteens(desired int, hand []cards.Card) []cards.Card {
 
 	return nil
+}
+
+func prioritize(c cards.Card, value int) int {
+	if c.Value == value {
+		return 1
+	}
+	return 0
 }
