cda25f7bcd0cda444beaba24c91b8f6ea1479f7f
cszczepaniak <cszczepaniak@gmail.com>
2019-10-22 22:08:16 -0500

    implement PegToFifteen

--- strategy/simple_peg.go
+++ strategy/simple_peg.go
@@ -4,8 +4,22 @@ import (
 	"github.com/joshprzybyszewski/cribbage/cards"
 )
 
+// PegToFifteen returns a card that yields a fifteen if it can
 func PegToFifteen(hand, prevPegs []cards.Card, curPeg int) (_ cards.Card, sayGo bool) {
-	return cards.Card{}, true
+	ct := 0
+	for _, c := range hand {
+		pegVal := c.PegValue() + curPeg
+		switch {
+		case pegVal == 15:
+			return c, false
+		case pegVal > 31:
+			ct++
+		}
+	}
+	if ct == len(hand) {
+		return cards.Card{}, true
+	}
+	return hand[0], false
 }
 
 func PegToThirtyOne(hand, prevPegs []cards.Card, curPeg int) (_ cards.Card, sayGo bool) {
