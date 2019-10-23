9e6720b4d2f89f5c8223bce9dadea5daed4d204b
joshprzybyszewski <josh.prezby@gmail.com>
2019-10-22 21:38:32 -0500

    Add some more tests

--- strategy/simple_peg_test.go
+++ strategy/simple_peg_test.go
@@ -23,6 +23,20 @@ func TestPegToFifteen(t *testing.T) {
 		inputCurPeg:   10,
 		expCard:       cards.NewCardFromString(`5s`),
 		expSayGo:      false,
+	}, {
+		msg:           `when can't, still returns a card`,
+		inputHand:     []string{`10c`},
+		inputPrevPegs: []string{`10s`, `3c`, `4d`, `5s`, `6h`, `3h`, `2h`},
+		inputCurPeg:   2,
+		expCard:       cards.NewCardFromString(`10c`),
+		expSayGo:      false,
+	}, {
+		msg:           `says go when can't play`,
+		inputHand:     []string{`3s`, `2h`, `4s`},
+		inputPrevPegs: []string{`10s`, `10h`, `10d`},
+		inputCurPeg:   30,
+		expCard:       cards.Card{},
+		expSayGo:      true,
 	}}
 
 	for _, tc := range testCases {
@@ -47,6 +61,34 @@ func TestPegToThirtyOne(t *testing.T) {
 		inputCurPeg:   30,
 		expCard:       cards.NewCardFromString(`ac`),
 		expSayGo:      false,
+	}, {
+		msg:           `another less obvious`,
+		inputHand:     []string{`7c`, `2h`, `4s`},
+		inputPrevPegs: []string{`10s`, `10h`, `4d`},
+		inputCurPeg:   24,
+		expCard:       cards.NewCardFromString(`7c`),
+		expSayGo:      false,
+	}, {
+		msg:           `when can't, still returns a card`,
+		inputHand:     []string{`10c`},
+		inputPrevPegs: []string{`10s`, `3c`, `4d`, `5s`, `6h`, `3h`, `2h`},
+		inputCurPeg:   2,
+		expCard:       cards.NewCardFromString(`10c`),
+		expSayGo:      false,
+	}, {
+		msg:           `distinguishes between 10s`,
+		inputHand:     []string{`10d`, `qh`, `ks`},
+		inputPrevPegs: []string{`qs`, `9h`},
+		inputCurPeg:   19,
+		expCard:       cards.NewCardFromString(`10d`), // chosen as the first card in the slice
+		expSayGo:      false,
+	}, {
+		msg:           `says go when can't play`,
+		inputHand:     []string{`3s`, `2h`, `4s`},
+		inputPrevPegs: []string{`10s`, `10h`, `10d`},
+		inputCurPeg:   30,
+		expCard:       cards.Card{},
+		expSayGo:      true,
 	}}
 
 	for _, tc := range testCases {
@@ -71,6 +113,27 @@ func TestPegToPair(t *testing.T) {
 		inputCurPeg:   3,
 		expCard:       cards.NewCardFromString(`3c`),
 		expSayGo:      false,
+	}, {
+		msg:           `when can't, still returns a card`,
+		inputHand:     []string{`10c`},
+		inputPrevPegs: []string{`10s`, `3c`, `4d`, `5s`, `6h`, `3h`, `2h`},
+		inputCurPeg:   2,
+		expCard:       cards.NewCardFromString(`10c`),
+		expSayGo:      false,
+	}, {
+		msg:           `distinguishes between 10s`,
+		inputHand:     []string{`10c`, `jd`, `qh`, `ks`},
+		inputPrevPegs: []string{`qs`},
+		inputCurPeg:   10,
+		expCard:       cards.NewCardFromString(`qh`),
+		expSayGo:      false,
+	}, {
+		msg:           `says go when can't play`,
+		inputHand:     []string{`3s`, `2h`, `4s`},
+		inputPrevPegs: []string{`10s`, `10h`, `10d`},
+		inputCurPeg:   30,
+		expCard:       cards.Card{},
+		expSayGo:      true,
 	}}
 
 	for _, tc := range testCases {
@@ -90,10 +153,38 @@ func TestPegToRun(t *testing.T) {
 		expSayGo      bool
 	}{{
 		msg:           `obvious`,
+		inputHand:     []string{`2d`, `8h`, `jc`},
+		inputPrevPegs: []string{`6d`, `7h`},
+		inputCurPeg:   14,
+		expCard:       cards.NewCardFromString(`8h`),
+		expSayGo:      false,
+	}, {
+		msg:           `less obvious`,
 		inputHand:     []string{`2d`, `7h`, `jc`},
-		inputPrevPegs: []string{`8h`, `6d`},
+		inputPrevPegs: []string{`6d`, `8h`},
 		inputCurPeg:   14,
 		expCard:       cards.NewCardFromString(`7h`),
+		expSayGo:      false,
+	}, {
+		msg:           `least obvious (chooses the longer run)`,
+		inputHand:     []string{`1d`, `4s`},
+		inputPrevPegs: []string{`5d`, `6h`, `3s`, `2d`},
+		inputCurPeg:   16,
+		expCard:       cards.NewCardFromString(`4s`),
+		expSayGo:      false,
+	}, {
+		msg:           `says go when can't play`,
+		inputHand:     []string{`3s`, `2h`, `4s`},
+		inputPrevPegs: []string{`10s`, `10h`, `10d`},
+		inputCurPeg:   30,
+		expCard:       cards.Card{},
+		expSayGo:      true,
+	}, {
+		msg:           `when can't, still returns a card`,
+		inputHand:     []string{`10c`},
+		inputPrevPegs: []string{`10s`, `3c`, `4d`, `5s`, `6h`, `3h`, `2h`},
+		inputCurPeg:   2,
+		expCard:       cards.NewCardFromString(`10c`),
 		expSayGo:      false,
 	}}
 
