1e888dc762d9863dd59160312665560cee0d2153
joshprzybyszewski <josh.prezby@gmail.com>
2019-10-23 22:07:02 -0500

    give the player the option to choose their opponent

--- play/play.go
+++ play/play.go
@@ -3,14 +3,15 @@ package play
 import (
 	"fmt"
 
+	survey "github.com/AlecAivazis/survey/v2"
+
 	"github.com/joshprzybyszewski/cribbage/game"
 )
 
 func PlayGame() error {
 	human := game.NewHumanPlayer(game.Blue)
-	// TODO ask the user for a difficulty...
-	// npc := game.NewDumbNPC(game.Red)
-	npc := game.NewSimpleNPC(game.Red)
+	npc := getOpponent(game.Red)
+
 	cfg := game.GameConfig{
 		Players:        []game.Player{human, npc},
 		StartingDealer: 0,
@@ -26,4 +27,28 @@ func PlayGame() error {
 	fmt.Printf("game over!\n")
 
 	return nil
+}
+
+func getOpponent(color game.PegColor) game.Player {
+	const dumb = `dumb`
+	const simple = `simple`
+	const calculated = `calculated`
+	opponentChoice := ``
+	prompt := &survey.Select{
+		Message: "Who would you like to play?",
+		Options: []string{dumb, simple, calculated},
+		Filter:  func(filter string, value string, index int) bool { return true },
+	}
+	survey.AskOne(prompt, &opponentChoice)
+
+	switch opponentChoice {
+	case dumb:
+		return game.NewDumbNPC(color)
+	case simple:
+		return game.NewSimpleNPC(color)
+	case calculated:
+		return game.NewSimpleNPC(color)
+	}
+
+	return game.NewDumbNPC(color)
 }
