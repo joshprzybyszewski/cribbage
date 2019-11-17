e14574ceffc3f633241cc858e44a8291848b4a2f
joshprzybyszewski <josh.prezby@gmail.com>
2019-11-16 21:35:35 -0600

    Give main a way to run the terminal client
    STATELESS

--- main.go
+++ main.go
@@ -3,15 +3,27 @@ package main
 import (
 	"flag"
 
+	"github.com/joshprzybyszewski/cribbage/local_client"
 	"github.com/joshprzybyszewski/cribbage/play"
 	"github.com/joshprzybyszewski/cribbage/server"
 )
 
 func main() {
 	legacy := flag.Bool(`legacy`, false, `set to true to play the legacy style game`)
+	client := flag.Bool(`client`, false, `set to true to talk to run as a terminal client against the server`)
+
+	flag.Parse()
 
 	if *legacy {
 		err := playLegacy()
+		if err != nil {
+			panic(err)
+		}
+		return
+	}
+
+	if *client {
+		err := runClient()
 		if err != nil {
 			panic(err)
 		}
@@ -31,6 +43,10 @@ func playServer() error {
 	}
 
 	return nil
+}
+
+func runClient() error {
+	return local_client.StartTerminalInteraction()
 }
 
 func playLegacy() error {
