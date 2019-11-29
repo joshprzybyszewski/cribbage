63e74e0db677571547a5eb2cf88e978a05814ca4
joshprzybyszewski <josh.prezby@gmail.com>
2019-11-29 07:39:07 -0600

    Improve the terminal players handling
    STATELESS

--- local_client/terminal_player.go
+++ local_client/terminal_player.go
@@ -29,9 +29,18 @@ type terminalClient struct {
 	myGames       map[model.GameID]model.Game
 }
 
+type termReqType int
+
+const (
+	blocking termReqType = iota
+	scoreUpdate
+	message
+)
+
 type terminalRequest struct {
 	gameID model.GameID
 	game   model.Game
+	req    termReqType
 	msg    string
 }
 
@@ -78,71 +87,9 @@ func StartTerminalInteraction() error {
 		router := gin.New()
 		router.Use(gin.LoggerWithWriter(playerServerFile), gin.Recovery())
 
-		router.POST("/blocking/:gameID", func(c *gin.Context) {
-			gIDStr := c.Param("gameID")
-			n, err := strconv.Atoi(gIDStr)
-			if err != nil {
-				c.String(http.StatusBadRequest, "Invalid GameID: %s", gIDStr)
-				return
-			}
-
-			msg := `We're told you're blocking: "`
-			reqBody, err := ioutil.ReadAll(c.Request.Body)
-			if err == nil {
-				msg += string(reqBody)
-			}
-			msg += `"`
-
-			reqChan <- terminalRequest{
-				gameID: model.GameID(n),
-				msg:    msg,
-			}
-
-			c.String(http.StatusOK, `received`)
-		})
-		router.POST("/message/:gameID", func(c *gin.Context) {
-			gIDStr := c.Param("gameID")
-			n, err := strconv.Atoi(gIDStr)
-			if err != nil {
-				c.String(http.StatusBadRequest, "Invalid GameID: %s", gIDStr)
-				return
-			}
-
-			msg := `Received Message: `
-			reqBody, err := ioutil.ReadAll(c.Request.Body)
-			if err != nil {
-				msg += `<unknown>`
-			} else {
-				msg += string(reqBody)
-			}
-
-			reqChan <- terminalRequest{
-				gameID: model.GameID(n),
-				msg:    msg,
-			}
-			c.String(http.StatusOK, `received`)
-		})
-		router.POST("/score/:gameID", func(c *gin.Context) {
-			gIDStr := c.Param("gameID")
-			n, err := strconv.Atoi(gIDStr)
-			if err != nil {
-				c.String(http.StatusBadRequest, "Invalid GameID: %s", gIDStr)
-				return
-			}
-
-			msg := `There's been a score update: "`
-			reqBody, err := ioutil.ReadAll(c.Request.Body)
-			if err == nil {
-				msg += string(reqBody)
-			}
-			msg += `"`
-
-			reqChan <- terminalRequest{
-				gameID: model.GameID(n),
-				msg:    msg,
-			}
-			c.String(http.StatusOK, `received`)
-		})
+		router.POST("/blocking/:gameID", handleBlocking(reqChan))
+		router.POST("/message/:gameID", handleMessage(reqChan))
+		router.POST("/score/:gameID", handleScoreUpdate(reqChan))
 
 		err = router.Run(fmt.Sprintf("0.0.0.0:%d", port)) // listen and serve on the addr
 		fmt.Printf("router.Run error: %+v\n", err)
@@ -175,6 +122,8 @@ func StartTerminalInteraction() error {
 
 		reqChan <- terminalRequest{
 			game: tc.myGames[tc.myCurrentGame],
+			msg:  `Starting terminal player`,
+			req:  message,
 		}
 		for req := range reqChan {
 			fmt.Printf("Message: \"%s\"\n", req.msg)
@@ -185,12 +134,23 @@ func StartTerminalInteraction() error {
 			if gID == model.InvalidGameID {
 				continue
 			}
-			err := tc.requestAndSendAction(gID)
-			if err != nil {
-				reqChan <- terminalRequest{
-					gameID: gID,
-					msg:    `Problem doing action. Try again?`,
+			switch req.req {
+			case blocking:
+				err := tc.requestAndSendAction(gID)
+				if err != nil {
+					reqChan <- terminalRequest{
+						gameID: gID,
+						msg:    `Problem doing action. Try again?`,
+						req:    message,
+					}
 				}
+			case message:
+				fmt.Println(req.msg)
+			case scoreUpdate:
+				fmt.Println(req.msg)
+				tc.printCurrentScore()
+			default:
+				fmt.Printf("Developer error: req needs a type %+v\n", req)
 			}
 		}
 	}()
@@ -199,6 +159,75 @@ func StartTerminalInteraction() error {
 	wg.Wait()
 
 	return nil
+}
+func handleBlocking(reqChan chan terminalRequest) func(*gin.Context) {
+	return func(c *gin.Context) {
+		gID, msg, err := getGameIDAndBody(c, `We heard you're blocking`)
+		if err != nil {
+			c.String(http.StatusBadRequest, err.Error())
+			return
+		}
+
+		reqChan <- terminalRequest{
+			gameID: gID,
+			msg:    msg,
+			req:    blocking,
+		}
+
+		c.String(http.StatusOK, `received`)
+	}
+}
+
+func handleMessage(reqChan chan terminalRequest) func(*gin.Context) {
+	return func(c *gin.Context) {
+		gID, msg, err := getGameIDAndBody(c, `Received a message`)
+		if err != nil {
+			c.String(http.StatusBadRequest, err.Error())
+			return
+		}
+
+		reqChan <- terminalRequest{
+			gameID: gID,
+			msg:    msg,
+			req:    message,
+		}
+		c.String(http.StatusOK, `received`)
+	}
+}
+
+func handleScoreUpdate(reqChan chan terminalRequest) func(*gin.Context) {
+	return func(c *gin.Context) {
+		gID, msg, err := getGameIDAndBody(c, `There was a score update`)
+		if err != nil {
+			c.String(http.StatusBadRequest, err.Error())
+			return
+		}
+
+		reqChan <- terminalRequest{
+			gameID: gID,
+			msg:    msg,
+			req:    scoreUpdate,
+		}
+		c.String(http.StatusOK, `received`)
+	}
+}
+
+func getGameIDAndBody(c *gin.Context, defBody string) (model.GameID, string, error) {
+	gIDStr := c.Param("gameID")
+	n, err := strconv.Atoi(gIDStr)
+	if err != nil {
+		return model.InvalidGameID, ``, fmt.Errorf("Invalid GameID: %s", gIDStr)
+	}
+
+	reqBody, err := ioutil.ReadAll(c.Request.Body)
+	var msg string
+	if err != nil || len(reqBody) == 0 {
+		msg = defBody
+	} else {
+		msg = string(reqBody)
+	}
+
+	return model.GameID(n), msg, nil
 }
 
 func (tc *terminalClient) makeRequest(method, apiURL string, data io.Reader) ([]byte, error) {
