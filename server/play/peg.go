3f70d21547d9e05f810ab2cfc2f5aa89acf70ea3
joshprzybyszewski <josh.prezby@gmail.com>
2019-11-29 08:39:03 -0600

    reduce cyclomatic complexity

--- server/play/peg.go
+++ server/play/peg.go
@@ -29,6 +29,7 @@ func (*peggingHandler) HandleAction(g *model.Game,
 	pAPIs map[model.PlayerID]interaction.Player,
 ) error {
 
+	// VALIDATE: check the action, then check the peg/go
 	if err := validateAction(g, action, model.PegCard); err != nil {
 		return err
 	}
@@ -39,70 +40,51 @@ func (*peggingHandler) HandleAction(g *model.Game,
 	}
 
 	pID := action.ID
-	if pa.SayGo {
-		curPeg := g.CurrentPeg()
-		minCardVal := minUnpeggedValue(g.Hands[pID], g.PeggedCards)
-		if curPeg+minCardVal <= model.MaxPeggingValue {
-			addPlayerToBlocker(g, pID, model.PegCard, pAPIs, `Cannot say go when has unpegged playable card`)
-			return nil
-		}
-	} else {
-		if !handContains(g.Hands[pID], pa.Card) {
-			addPlayerToBlocker(g, pID, model.PegCard, pAPIs, `Cannot peg card you don't have`)
-			return nil
-		}
-
-		if hasBeenPegged(g.PeggedCards, pa.Card) {
-			addPlayerToBlocker(g, pID, model.PegCard, pAPIs, `Cannot peg same card twice`)
-			return nil
-		}
-
-		if g.CurrentPeg()+pa.Card.PegValue() > model.MaxPeggingValue {
-			addPlayerToBlocker(g, pID, model.PegCard, pAPIs, `Cannot peg card with this value`)
-			return nil
-		}
+	if err := validatePegAction(g, pID, pa); err != nil {
+		addPlayerToBlocker(g, pID, model.PegCard, pAPIs, err.Error())
+		return nil
 	}
 
+	// CLEAN: remove this player from the blockers
 	if len(g.BlockingPlayers) != 1 {
 		log.Printf("Expected one blocker for pegging, but had: %+v\n", g.BlockingPlayers)
 	}
 	removePlayerFromBlockers(g, action)
 
+	// ACT: do the "say go" or peg
 	if pa.SayGo {
 		doSayGo(g, action, pAPIs)
-	} else {
-		err := doPeg(g, action, pa, pAPIs)
-		if err != nil {
-			return err
+	} else if err := doPeg(g, action, pa, pAPIs); err != nil {
+		return err
+	}
+
+	// PROGRESS: move the game state forward appropriately
+	progressAfterPeg(g, action, pAPIs)
+
+	return nil
+}
+
+func validatePegAction(g *model.Game, pID model.PlayerID, pa model.PegAction) error {
+	if pa.SayGo {
+		curPeg := g.CurrentPeg()
+		minCardVal := minUnpeggedValue(g.Hands[pID], g.PeggedCards)
+		if curPeg+minCardVal <= model.MaxPeggingValue {
+			return errors.New(`Cannot say go when has unpegged playable card`)
 		}
-	}
-
-	if g.IsOver() {
 		return nil
 	}
 
-	if len(g.PeggedCards) == 4*len(g.Players) {
-		// This was the last card: give one point to this player.
-		addPoints(g, action.ID, 1, pAPIs, `last card`)
-		return nil
+	if !handContains(g.Hands[pID], pa.Card) {
+		return errors.New(`Cannot peg card you don't have`)
 	}
 
-	if g.IsOver() {
-		return nil
+	if hasBeenPegged(g.PeggedCards, pa.Card) {
+		return errors.New(`Cannot peg same card twice`)
 	}
 
-	// TODO this might need to be the next player with cards still to peg
-	// Set the next player to peg as the blocker
-	nextPlayerIndex := -1
-	for i, p := range g.Players {
-		if p.ID == action.ID {
-			nextPlayerIndex = (i + 1) % len(g.Players)
-			break
-		}
+	if g.CurrentPeg()+pa.Card.PegValue() > model.MaxPeggingValue {
+		return errors.New(`Cannot peg card with this value`)
 	}
-
-	bp := g.Players[nextPlayerIndex]
-	addPlayerToBlocker(g, bp.ID, model.PegCard, pAPIs, ``)
 
 	return nil
 }
@@ -144,4 +126,36 @@ func doSayGo(g *model.Game,
 		// The go's went all the way around. Take a point
 		addPoints(g, action.ID, 1, pAPIs, `the go`)
 	}
+}
+
+func progressAfterPeg(
+	g *model.Game,
+	action model.PlayerAction,
+	pAPIs map[model.PlayerID]interaction.Player,
+) {
+
+	if g.IsOver() {
+		// we shouldn't do anything if the game is over
+		return
+	}
+
+	if len(g.PeggedCards) == 4*len(g.Players) {
+		// This was the last card: give one point to this player.
+		addPoints(g, action.ID, 1, pAPIs, `last card`)
+		return
+	}
+
+	// Set the next player to peg as the blocker
+	// If we don't require everyone to say go, then we'll need to change the logic
+	// in game.CurrentPeg
+	nextPlayerIndex := -1
+	for i, p := range g.Players {
+		if p.ID == action.ID {
+			nextPlayerIndex = (i + 1) % len(g.Players)
+			break
+		}
+	}
+
+	bp := g.Players[nextPlayerIndex]
+	addPlayerToBlocker(g, bp.ID, model.PegCard, pAPIs, ``)
 }
