40738420107ef62c87fc1208998a2d68e1db0e07
joshprzybyszewski <josh.prezby@gmail.com>
2019-12-03 21:17:35 -0600

    Set up custom registry
    so that our maps can be fine

--- model/model.go
+++ model/model.go
@@ -10,9 +10,9 @@ const (
 )
 
 type Card struct {
-	Suit Suit `protobuf:"varint,1,req,name=suit,proto3" json:"suit"` //nolint:lll
+	Suit Suit `protobuf:"varint,1,req,name=suit,proto3" json:"suit" bson:"suit"` //nolint:lll
 	// Ace is 1, King is 13
-	Value int `protobuf:"varint,2,req,name=value,proto3" json:"value"` //nolint:lll
+	Value int `protobuf:"varint,2,req,name=value,proto3" json:"value" bson:"value"` //nolint:lll
 }
 
 const NumCardsPerDeck = 52
@@ -20,8 +20,8 @@ const JackValue = 11 // Ace is 1, King is 13
 
 type PeggedCard struct {
 	Card
-	Action   int      `protobuf:"varint,3,req,name=action,proto3" json:"action"`     //nolint:lll
-	PlayerID PlayerID `protobuf:"varint,4,req,name=playerID,proto3" json:"playerID"` //nolint:lll
+	Action   int      `protobuf:"varint,3,req,name=action,proto3" json:"action" bson:"action"`       //nolint:lll
+	PlayerID PlayerID `protobuf:"varint,4,req,name=playerID,proto3" json:"playerID" bson:"playerID"` //nolint:lll
 }
 
 type PlayerID string
@@ -53,14 +53,14 @@ func (c PlayerColor) String() string {
 }
 
 type Player struct {
-	ID    PlayerID               `protobuf:"varint,1,req,name=id,proto3" json:"id"`                              //nolint:lll
-	Name  string                 `protobuf:"string,2,req,name=name,proto3" json:"name"`                          //nolint:lll
-	Games map[GameID]PlayerColor `protobuf:"map<varint, varint>,3,opt,name=games,proto3" json:"games,omitempty"` //nolint:lll
+	ID    PlayerID               `protobuf:"varint,1,req,name=id,proto3" json:"id" bson:"id"`                                 //nolint:lll
+	Name  string                 `protobuf:"string,2,req,name=name,proto3" json:"name" bson:"name"`                           //nolint:lll
+	Games map[GameID]PlayerColor `protobuf:"map<varint, varint>,3,opt,name=games,proto3" json:"games,omitempty" bson:"games"` //nolint:lll
 }
 
 type InteractionMeans struct {
-	Means string      `protobuf:"-" json:"-"`
-	Info  interface{} `protobuf:"-" json:"-"`
+	Means string      `protobuf:"-" json:"-" bson:"-"`
+	Info  interface{} `protobuf:"-" json:"-" bson:"-"`
 }
 
 type Blocker int
@@ -75,41 +75,41 @@ const (
 )
 
 type CribBlocker struct {
-	Desired      int                      `protobuf:"varint,1,req,name=desired,proto3" json:"desired"`                                 //nolint:lll//nolint:lll
-	Dealer       PlayerID                 `protobuf:"varint,2,req,name=playerID,proto3" json:"playerID"`                               //nolint:lll//nolint:lll
-	PlayerColors map[PlayerID]PlayerColor `protobuf:"map<varint,varint>,3,opt,name=playerColors,proto3" json:"playerColors,omitempty"` //nolint:lll//nolint:lll
+	Desired      int                      `protobuf:"varint,1,req,name=desired,proto3" json:"desired" bson:"desired"`                                                //nolint:lll//nolint:lll
+	Dealer       PlayerID                 `protobuf:"varint,2,req,name=playerID,proto3" json:"playerID" bson:"playerID"`                                             //nolint:lll//nolint:lll
+	PlayerColors map[PlayerID]PlayerColor `protobuf:"map<varint,varint>,3,opt,name=playerColors,proto3" json:"playerColors,omitempty" bson:"playerColors,omitempty"` //nolint:lll//nolint:lll
 }
 
 type PlayerAction struct {
-	GameID    GameID      `protobuf:"varint,1,req,name=gameID,proto3" json:"gameID"`       //nolint:lll
-	ID        PlayerID    `protobuf:"varint,2,req,name=id,proto3" json:"id"`               //nolint:lll
-	Overcomes Blocker     `protobuf:"varint,3,req,name=overcomes,proto3" json:"overcomes"` //nolint:lll
-	Action    interface{} `protobuf:"-" json:"action"`                                     //nolint:lll
+	GameID    GameID      `protobuf:"varint,1,req,name=gameID,proto3" json:"gameID" bson:"gameID"`          //nolint:lll
+	ID        PlayerID    `protobuf:"varint,2,req,name=id,proto3" json:"id" bson:"id"`                      //nolint:lll
+	Overcomes Blocker     `protobuf:"varint,3,req,name=overcomes,proto3" json:"overcomes" bson:"overcomes"` //nolint:lll
+	Action    interface{} `protobuf:"-" json:"action" bson:"action"`                                        //nolint:lll
 }
 
 type DealAction struct {
-	NumShuffles int `protobuf:"varint,1,req,name=numShuffles,proto3" json:"numShuffles"` //nolint:lll
+	NumShuffles int `protobuf:"varint,1,req,name=numShuffles,proto3" json:"numShuffles" bson:"numShuffles"` //nolint:lll
 }
 
 type BuildCribAction struct {
-	Cards []Card `protobuf:"Card,rep,1,req,name=cards,proto3" json:"cards"` //nolint:lll
+	Cards []Card `protobuf:"Card,rep,1,req,name=cards,proto3" json:"cards" bson:"cards"` //nolint:lll
 }
 
 type CutDeckAction struct {
-	Percentage float64 `protobuf:"varint,1,req,name=percentage,proto3" json:"percentage"` //nolint:lll
+	Percentage float64 `protobuf:"varint,1,req,name=percentage,proto3" json:"percentage" bson:"percentage"` //nolint:lll
 }
 
 type PegAction struct {
-	Card  Card `protobuf:"Card,1,req,name=card,proto3" json:"card"`   //nolint:lll
-	SayGo bool `protobuf:"bool,2,req,name=sayGo,proto3" json:"sayGo"` //nolint:lll
+	Card  Card `protobuf:"Card,1,req,name=card,proto3" json:"card" bson:"card"`    //nolint:lll
+	SayGo bool `protobuf:"bool,2,req,name=sayGo,proto3" json:"sayGo" bson:"sayGo"` //nolint:lll
 }
 
 type CountHandAction struct {
-	Pts int `protobuf:"varint,1,req,name=pts,proto3" json:"pts"` //nolint:lll
+	Pts int `protobuf:"varint,1,req,name=pts,proto3" json:"pts" bson:"pts"` //nolint:lll
 }
 
 type CountCribAction struct {
-	Pts int `protobuf:"varint,1,req,name=pts,proto3" json:"pts"` //nolint:lll
+	Pts int `protobuf:"varint,1,req,name=pts,proto3" json:"pts" bson:"pts"` //nolint:lll
 }
 
 type Phase int
@@ -140,19 +140,19 @@ const (
 )
 
 type Game struct {
-	ID              GameID                   `protobuf:"varint,1,name=id,proto3" json:"id"`                                                      //nolint:lll
-	Players         []Player                 `protobuf:"bytes,3,rep,name=players,proto3,proto3" json:"players"`                                  //nolint:lll
-	Deck            Deck                     `protobuf:"-" json:"-"`                                                                             //nolint:lll
-	BlockingPlayers map[PlayerID]Blocker     `protobuf:"map<varint, varint>,4,opt,name=blockingPlayers,proto3" json:"blockingPlayers,omitempty"` //nolint:lll
-	CurrentDealer   PlayerID                 `protobuf:"varint,5,opt,name=currentDealer,proto3" json:"currentDealer"`                            //nolint:lll
-	PlayerColors    map[PlayerID]PlayerColor `protobuf:"map<varint, varint>,6,name=playerColors,proto3" json:"playerColors,omitempty"`           //nolint:lll
-	CurrentScores   map[PlayerColor]int      `protobuf:"map<varint, varint>,7,opt,name=currentScores,proto3" json:"currentScores"`               //nolint:lll
-	LagScores       map[PlayerColor]int      `protobuf:"map<varint, varint>,8,opt,name=lagScores,proto3" json:"lagScores"`                       //nolint:lll
-	Phase           Phase                    `protobuf:"varint,9,opt,name=phase,proto3" json:"phase"`                                            //nolint:lll
-	Hands           map[PlayerID][]Card      `protobuf:"map<varint, bytes>,10,opt,name=hands,proto3" json:"hands,omitempty"`                     //nolint:lll
-	CutCard         Card                     `protobuf:"Card,11,opt,name=cutCard,proto3" json:"cutCard,omitempty"`                               //nolint:lll
-	Crib            []Card                   `protobuf:"Card,12,rep,name=crib,proto3" json:"crib,omitempty"`                                     //nolint:lll
-	PeggedCards     []PeggedCard             `protobuf:"PeggedCard,13,rep,opt,name=peggedCards,proto3" json:"peggedCards,omitempty"`             //nolint:lll
+	ID              GameID                   `protobuf:"varint,1,name=id,proto3" json:"id" bson:"id"`                                                                             //nolint:lll
+	Players         []Player                 `protobuf:"bytes,3,rep,name=players,proto3,proto3" json:"players" bson:"players"`                                                    //nolint:lll
+	Deck            Deck                     `protobuf:"-" json:"-" bson:"-"`                                                                                                     //nolint:lll
+	BlockingPlayers map[PlayerID]Blocker     `protobuf:"map<varint, varint>,4,opt,name=blockingPlayers,proto3" json:"blockingPlayers,omitempty" bson:"blockingPlayers,omitempty"` //nolint:lll
+	CurrentDealer   PlayerID                 `protobuf:"varint,5,opt,name=currentDealer,proto3" json:"currentDealer" bson:"currentDealer"`                                        //nolint:lll
+	PlayerColors    map[PlayerID]PlayerColor `protobuf:"map<varint, varint>,6,name=playerColors,proto3" json:"playerColors,omitempty" bson:"playerColors,omitempty"`              //nolint:lll
+	CurrentScores   map[PlayerColor]int      `protobuf:"map<varint, varint>,7,opt,name=currentScores,proto3" json:"currentScores" bson:"currentScores"`                           //nolint:lll
+	LagScores       map[PlayerColor]int      `protobuf:"map<varint, varint>,8,opt,name=lagScores,proto3" json:"lagScores" bson:"lagScores"`                                       //nolint:lll
+	Phase           Phase                    `protobuf:"varint,9,opt,name=phase,proto3" json:"phase" bson:"phase"`                                                                //nolint:lll
+	Hands           map[PlayerID][]Card      `protobuf:"map<varint, bytes>,10,opt,name=hands,proto3" json:"hands,omitempty" bson:"hands,omitempty"`                               //nolint:lll
+	CutCard         Card                     `protobuf:"Card,11,opt,name=cutCard,proto3" json:"cutCard,omitempty" bson:"cutCard,omitempty"`                                       //nolint:lll
+	Crib            []Card                   `protobuf:"Card,12,rep,name=crib,proto3" json:"crib,omitempty" bson:"crib,omitempty"`                                                //nolint:lll
+	PeggedCards     []PeggedCard             `protobuf:"PeggedCard,13,rep,opt,name=peggedCards,proto3" json:"peggedCards,omitempty" bson:"peggedCards,omitempty"`                 //nolint:lll
 
 	actions []PlayerAction
 }
---
+++ server/persistence/mongodb/qa/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joshprzybyszewski/cribbage/model"
+	"github.com/joshprzybyszewski/cribbage/server/persistence/mongodb"
+)
+
+func main() {
+	mdb, err := mongodb.New(`mongodb://localhost:27017`)
+	if err != nil {
+		fmt.Printf("Error on New: %+v\n", err)
+		os.Exit(1)
+	}
+
+	id := 47
+	idStr := fmt.Sprintf("%d", id)
+	otherIDStr := fmt.Sprintf("%d", id*2)
+	g1ID := model.GameID(id)
+	pID := model.PlayerID(idStr)
+	josh := model.Player{
+		ID:   pID,
+		Name: idStr,
+		Games: map[model.GameID]model.PlayerColor{
+			g1ID:               model.Blue,
+			model.GameID(5555): model.Red,
+			model.GameID(9876): model.Green,
+		},
+	}
+	ellen := model.Player{
+		ID:   model.PlayerID(otherIDStr),
+		Name: otherIDStr,
+		Games: map[model.GameID]model.PlayerColor{
+			g1ID:               model.Red,
+			model.GameID(5555): model.Blue,
+			model.GameID(9876): model.Red,
+		},
+	}
+	fmt.Printf("Creating josh: %+v\n", josh)
+	err = mdb.CreatePlayer(josh)
+	if err != nil {
+		fmt.Printf("Error on CreatePlayer: %+v\n", err)
+		os.Exit(1)
+	}
+
+	p, err := mdb.GetPlayer(pID)
+	if err != nil {
+		fmt.Printf("Error on GetPlayer: %+v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Got Player: %+v\n", p)
+
+	g := model.Game{
+		ID:              g1ID,
+		Players:         []model.Player{josh, ellen},
+		Deck:            model.NewDeck(),
+		BlockingPlayers: map[model.PlayerID]model.Blocker{ellen.ID: model.CountHand},
+		CurrentDealer:   josh.ID,
+		PlayerColors:    map[model.PlayerID]model.PlayerColor{josh.ID: model.Blue, ellen.ID: model.Red},
+		CurrentScores:   map[model.PlayerColor]int{model.Blue: 0, model.Red: 0},
+		LagScores:       map[model.PlayerColor]int{model.Blue: 0, model.Red: 0},
+		Phase:           model.Counting,
+		Hands: map[model.PlayerID][]model.Card{
+			josh.ID: {
+				model.NewCardFromString(`7s`),
+				model.NewCardFromString(`8s`),
+				model.NewCardFromString(`9s`),
+				model.NewCardFromString(`10s`),
+			},
+			ellen.ID: {
+				model.NewCardFromString(`7c`),
+				model.NewCardFromString(`8c`),
+				model.NewCardFromString(`9c`),
+				model.NewCardFromString(`10c`),
+			},
+		},
+		CutCard:     model.NewCardFromString(`7h`),
+		Crib:        make([]model.Card, 4),
+		PeggedCards: make([]model.PeggedCard, 0, 8),
+	}
+
+	fmt.Printf("Made Game: %+v\n", g)
+	err = mdb.SaveGame(g)
+	if err != nil {
+		fmt.Printf("Error on SaveGame: %+v\n", err)
+		os.Exit(1)
+	}
+
+	gDB, err := mdb.GetGame(g1ID)
+	if err != nil {
+		fmt.Printf("Error on GetGame: %+v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Got Game:  %+v\n", gDB)
+
+}
