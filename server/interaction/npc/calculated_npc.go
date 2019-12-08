c035b16b298cf476074b8dd9713fe392a54ce81f
cszczepaniak <cszczepaniak@gmail.com>
2019-12-07 23:34:42 -0600

    port calculated NPC

---
+++ server/interaction/npc/calculated_npc.go
@@ -0,0 +1,30 @@
+package npc
+
+import (
+	"github.com/joshprzybyszewski/cribbage/logic/strategy"
+	"github.com/joshprzybyszewski/cribbage/model"
+	"github.com/joshprzybyszewski/cribbage/utils/rand"
+)
+
+var _ npcLogic = (*calcNPCInteraction)(nil)
+
+type calcNPCInteraction struct{}
+
+func (npc *calcNPCInteraction) addToCrib(g model.Game, pID model.PlayerID, n int) []model.Card {
+	hand := g.Hands[pID]
+	if pID == g.CurrentDealer {
+		if rand.Int()%2 == 0 {
+			return strategy.KeepHandLowestPotential(n, hand)
+		}
+		return strategy.GiveCribHighestPotential(n, hand)
+	}
+
+	if rand.Int()%2 == 0 {
+		return strategy.KeepHandHighestPotential(n, hand)
+	}
+	return strategy.GiveCribLowestPotential(n, hand)
+}
+
+func (npc *calcNPCInteraction) peg(g model.Game, pID model.PlayerID) (model.Card, bool) {
+	return strategy.PegHighestCardNow(g.Hands[pID], g.PeggedCards, g.CurrentPeg())
+}
