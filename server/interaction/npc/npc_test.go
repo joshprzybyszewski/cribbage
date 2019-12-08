076aadcaed75e0d004b1a9324ca1f240a02d7406
cszczepaniak <cszczepaniak@gmail.com>
2019-12-08 00:43:37 -0600

    update tests

--- server/interaction/npc/npc_test.go
+++ server/interaction/npc/npc_test.go
@@ -1,117 +1,115 @@
 package npc
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/joshprzybyszewski/cribbage/model"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func Test_npcPlayer_buildDealAction(t *testing.T) {
+func createPlayer(t *testing.T, pID model.PlayerID) *npcPlayer {
+	npc, err := NewNPCPlayer(pID, func(a model.PlayerAction) error {
+		return nil
+	})
+	require.Nil(t, err)
+	p, ok := npc.(*npcPlayer)
+	assert.True(t, ok)
+	return p
+}
+
+func newNPlayerGame(npcID model.PlayerID, n int) model.Game {
+	players := make([]model.Player, n)
+	for i := 0; i < n-1; i++ {
+		id := model.PlayerID(fmt.Sprintf(`p%d`, i))
+		players[i] = model.Player{ID: id}
+	}
+	players[len(players)-1] = model.Player{ID: npcID}
+
+	hands := make(map[model.PlayerID][]model.Card)
+	nCards := 6
+	switch n {
+	case 3, 4:
+		nCards = 5
+	}
+	for _, p := range players {
+		hands[p.ID] = make([]model.Card, nCards)
+	}
+	return model.Game{
+		Players: players,
+		Hands:   hands,
+	}
+}
+
+func TestBuildDealAction(t *testing.T) {
 	tests := []struct {
 		desc string
-		npc  Mode
+		npc  model.PlayerID
 		g    model.Game
 		exp  model.PlayerAction
 	}{{
 		desc: `test dumb npc`,
-		npc:  Dumb,
+		npc:  `dumbNPC`,
 	}, {
 		desc: `test simple npc`,
-		npc:  Simple,
+		npc:  `simpleNPC`,
 	}, {
 		desc: `test calculated npc`,
-		npc:  Calculated,
+		npc:  `calculatedNPC`,
 	}}
 	for _, tc := range tests {
-		npc := npcPlayer{
-			Mode: tc.npc,
-		}
-		a := npc.buildAction(model.DealCards, tc.g)
+		p := createPlayer(t, tc.npc)
+
+		a := p.buildAction(model.DealCards, tc.g)
 		assert.Equal(t, a.Overcomes, model.DealCards)
 
 		da, ok := a.Action.(model.DealAction)
 		assert.True(t, ok)
-		assert.Less(t, da.NumShuffles, 10)
+		assert.LessOrEqual(t, da.NumShuffles, 10)
 		assert.GreaterOrEqual(t, da.NumShuffles, 1)
 	}
 }
-func Test_npcPlayer_buildBuildCribAction(t *testing.T) {
+func TestBuildBuildCribAction(t *testing.T) {
 	tests := []struct {
 		desc      string
-		npc       Mode
+		npc       model.PlayerID
 		g         model.Game
 		expNCards int
 	}{{
 		desc:      `test dumb npc`,
-		npc:       Dumb,
-		g:         model.Game{},
+		npc:       `dumbNPC`,
+		g:         newNPlayerGame(`dumbNPC`, 2),
 		expNCards: 2,
 	}, {
 		desc:      `test simple npc`,
-		npc:       Simple,
-		g:         model.Game{},
+		npc:       `simpleNPC`,
+		g:         newNPlayerGame(`simpleNPC`, 2),
 		expNCards: 2,
 	}, {
 		desc:      `test calculated npc`,
-		npc:       Calculated,
-		g:         model.Game{},
+		npc:       `calculatedNPC`,
+		g:         newNPlayerGame(`calculatedNPC`, 2),
 		expNCards: 2,
 	}, {
-		desc: `test 3 player game`,
-		npc:  Calculated,
-		g: model.Game{
-			Players: make([]model.Player, 3),
-		},
+		desc:      `test 3 player game`,
+		npc:       `dumbNPC`,
+		g:         newNPlayerGame(`dumbNPC`, 3),
 		expNCards: 1,
 	}, {
-		desc: `test 4 player game`,
-		npc:  Calculated,
-		g: model.Game{
-			Players: make([]model.Player, 4),
-		},
+		desc:      `test 4 player game`,
+		npc:       `dumbNPC`,
+		g:         newNPlayerGame(`dumbNPC`, 4),
 		expNCards: 1,
 	}}
 	for _, tc := range tests {
-		npc := npcPlayer{
-			Mode: tc.npc,
-		}
-		a := npc.buildAction(model.CribCard, tc.g)
+		p := createPlayer(t, tc.npc)
+
+		a := p.buildAction(model.CribCard, tc.g)
 		assert.Equal(t, a.Overcomes, model.CribCard)
 
 		bca, ok := a.Action.(model.BuildCribAction)
 		assert.True(t, ok)
 		assert.Len(t, bca.Cards, tc.expNCards)
-	}
-}
-
-func TestNewNPCPlayer(t *testing.T) {
-	tests := []struct {
-		desc  string
-		n     Mode
-		expID model.PlayerID
-	}{
-		{
-			desc:  `test dumb npc`,
-			n:     Dumb,
-			expID: `dumbNPC`,
-		},
-		{
-			desc:  `test simple npc`,
-			n:     Simple,
-			expID: `simpleNPC`,
-		},
-		{
-			desc:  `test calculated npc`,
-			n:     Calculated,
-			expID: `calculatedNPC`,
-		},
-	}
-	f := func(a model.PlayerAction) error {
-		return nil
-	}
-	for _, tc := range tests {
-		n := NewNPCPlayer(tc.n, f)
-		assert.Equal(t, n.ID(), tc.expID)
 	}
 }
