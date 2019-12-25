a97f89596f428fb8dee49d663d7031b43d0118ac
joshprzybyszewski <josh.prezby@gmail.com>
2019-12-25 07:52:13 -0600

    Update wasm files

--- wasm/wasm_main.go
+++ wasm/wasm_main.go
@@ -4,8 +4,31 @@ package main
 
 import (
 	"fmt"
+	"syscall/js"
 )
 
 func main() {
 	fmt.Println("hello wasm")
+
+	listenToHome()
+	listenForGamePage()
+
+	println(`exiting main`)
+}
+
+func listenToHome() {
+	println(`listenToHome`)
+
+	cb := js.NewCallback(func(args []js.Value) {
+		println(`wasm callback`)
+	})
+	// TODO cb.Release()
+
+	js.Global().Get("document").
+		Call("getElementById", "create-user-submit").
+		Call("addEventListener", "submit", cb)
+}
+
+func listenForGamePage() {
+	println(`listenForGamePage`)
 }
