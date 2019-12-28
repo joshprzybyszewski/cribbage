65fe8d026d5e26fa88886e78823aa2a378c73424
cszczepaniak <cszczepaniak@gmail.com>
2019-12-28 17:32:02 -0600

    fix bug where calc npc wouldn't peg

--- logic/strategy/calculated_peg.go
+++ logic/strategy/calculated_peg.go
@@ -6,15 +6,13 @@ import (
 )
 
 func PegHighestCardNow(hand []model.Card, prevPegs []model.PeggedCard, curPeg int) (model.Card, bool) {
-	if curPeg == 0 {
-		return hand[0], false
-	}
-
 	bestCard := model.Card{}
-	bestPoints := 0
+	bestPoints := -1
+	cardsOverMax := 0
 
 	for _, c := range hand {
 		if curPeg+c.PegValue() > 31 {
+			cardsOverMax++
 			continue
 		}
 
@@ -28,6 +26,8 @@ func PegHighestCardNow(hand []model.Card, prevPegs []model.PeggedCard, curPeg in
 			bestPoints = p
 		}
 	}
-
-	return bestCard, bestCard == model.Card{}
+	if cardsOverMax == len(hand) {
+		return model.Card{}, true
+	}
+	return bestCard, false
 }
