b1e53704e1ab5bc44af0489454b297029634d429
cszczepaniak <cszczepaniak@gmail.com>
2020-01-01 10:23:31 -0600

    delete legacy code :#

--- main.go
+++ main.go
@@ -4,26 +4,16 @@ import (
 	"flag"
 
 	"github.com/joshprzybyszewski/cribbage/localclient"
-	"github.com/joshprzybyszewski/cribbage/play"
 	"github.com/joshprzybyszewski/cribbage/server"
 )
 
 var (
-	legacy = flag.Bool(`legacy`, false, `set to true to play the legacy style game`)
 	client = flag.Bool(`client`, false, `set to true to talk to run as a terminal client against the server`)
 )
 
 func main() {
 
 	flag.Parse()
-
-	if *legacy {
-		err := playLegacy()
-		if err != nil {
-			panic(err)
-		}
-		return
-	}
 
 	if *client {
 		err := runClient()
@@ -50,8 +40,4 @@ func playServer() error {
 
 func runClient() error {
 	return localclient.StartTerminalInteraction()
-}
-
-func playLegacy() error {
-	return play.PlayGame()
 }
