f32076b2652b0a89619899fe318415bedccd643a
cszczepaniak <cszczepaniak@gmail.com>
2020-01-04 16:43:10 -0600

    finish UpdateGameColor refactor

--- server/persistence/mysql/mysql_service_player.go
+++ server/persistence/mysql/mysql_service_player.go
@@ -12,6 +12,55 @@ import (
 	"github.com/go-sql-driver/mysql"
 )
 
+type dbPlayer struct {
+	id      model.PlayerID
+	name    string
+	gIDJson []byte
+}
+
+func playerToDBPlayer(p model.Player) (dbPlayer, error) {
+	res := dbPlayer{
+		id:   p.ID,
+		name: p.Name,
+	}
+	if len(p.Games) > 0 {
+		gIDs := make([]model.GameID, 0, len(p.Games))
+		for gID := range p.Games {
+			gIDs = append(gIDs, gID)
+		}
+		j, err := json.Marshal(gIDs)
+		if err != nil {
+			return dbPlayer{}, err
+		}
+		res.gIDJson = j
+	}
+	return res, nil
+}
+
+func dbPlayerToPlayer(p dbPlayer, gs gameService) (model.Player, error) {
+	res := model.Player{
+		ID:   p.id,
+		Name: p.name,
+	}
+	var gameIDs []model.GameID
+	if len(p.gIDJson) > 0 {
+		err := json.Unmarshal(p.gIDJson, &gameIDs)
+		if err != nil {
+			return model.Player{}, err
+		}
+		gameMap := make(map[model.GameID]model.PlayerColor, len(gameIDs))
+		for _, id := range gameIDs {
+			g, err := gs.Get(id)
+			if err != nil {
+				return model.Player{}, err
+			}
+			gameMap[id] = g.PlayerColors[res.ID]
+		}
+		res.Games = gameMap
+	}
+	return res, nil
+}
+
 var _ persistence.PlayerService = (*playerService)(nil)
 
 type playerService struct {
@@ -27,18 +76,16 @@ func getPlayerService(ctx context.Context, db *sql.DB) (*playerService, error) {
 	}, nil
 }
 
+func (ps *playerService) beginTx() (*sql.Tx, error) {
+	// https://en.wikipedia.org/wiki/Isolation_(database_systems)#Isolation_levels
+	// let's use the highest level of isolation for now (which apparently uses read and write locks)
+	return ps.db.BeginTx(ps.ctx, &sql.TxOptions{Isolation: sql.LevelSerializable})
+}
+
 func (ps *playerService) Create(p model.Player) error {
-	var gIDJson []byte
-	if len(p.Games) > 0 {
-		ids := make([]model.GameID, 0, len(p.Games))
-		for id := range p.Games {
-			ids = append(ids, id)
-		}
-		j, err := json.Marshal(ids)
-		if err != nil {
-			return err
-		}
-		gIDJson = j
+	dbp, err := playerToDBPlayer(p)
+	if err != nil {
+		return err
 	}
 
 	tx, err := ps.beginTx()
@@ -54,7 +101,7 @@ func (ps *playerService) Create(p model.Player) error {
 		return err
 	}
 	defer stmt.Close()
-	_, err = stmt.ExecContext(ps.ctx, p.ID, p.Name, gIDJson)
+	_, err = stmt.ExecContext(ps.ctx, dbp.id, dbp.name, dbp.gIDJson)
 
 	if err != nil {
 		if me, ok := err.(*mysql.MySQLError); !ok {
@@ -72,8 +119,6 @@ func (ps *playerService) Create(p model.Player) error {
 }
 
 func (ps *playerService) Get(id model.PlayerID) (model.Player, error) {
-	result := model.Player{}
-	var gameIDs []model.GameID
 	tx, err := ps.beginTx()
 	if err != nil {
 		return model.Player{}, err
@@ -88,8 +133,8 @@ func (ps *playerService) Get(id model.PlayerID) (model.Player, error) {
 
 	r := stmt.QueryRowContext(ps.ctx, id)
 
-	var gIDJson []byte
-	err = r.Scan(&result.ID, &result.Name, &gIDJson)
+	dbp := dbPlayer{}
+	err = r.Scan(&dbp.id, &dbp.name, &dbp.gIDJson)
 	if err != nil {
 		if err != sql.ErrNoRows {
 			return model.Player{}, err
@@ -101,23 +146,11 @@ func (ps *playerService) Get(id model.PlayerID) (model.Player, error) {
 	if err != nil {
 		return model.Player{}, err
 	}
-	if len(gIDJson) > 0 {
-		err = json.Unmarshal(gIDJson, &gameIDs)
-		if err != nil {
-			return model.Player{}, err
-		}
-		gameMap := make(map[model.GameID]model.PlayerColor, len(gameIDs))
-		for _, id := range gameIDs {
-			g, err := ps.gs.Get(id)
-			if err != nil {
-				return model.Player{}, err
-			}
-			gameMap[id] = g.PlayerColors[result.ID]
-		}
-		result.Games = gameMap
+	p, err := dbPlayerToPlayer(dbp, ps.gs)
+	if err != nil {
+		return model.Player{}, err
 	}
-
-	return result, nil
+	return p, nil
 }
 
 func (ps *playerService) UpdateGameColor(pID model.PlayerID, gID model.GameID, color model.PlayerColor) error {
@@ -138,12 +171,32 @@ func (ps *playerService) UpdateGameColor(pID model.PlayerID, gID model.GameID, c
 		// Nothing to do; the player already knows its color
 		return nil
 	}
+	p.Games[gID] = color
 
+	dbp, err := playerToDBPlayer(p)
+	if err != nil {
+		return err
+	}
+
+	tx, err := ps.beginTx()
+	if err != nil {
+		return err
+	}
+
+	stmt, err := tx.PrepareContext(ps.ctx, `UPDATE `+playerTableName+` SET gameIDs = ? WHERE id = ?`)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.ExecContext(ps.ctx, dbp.gIDJson, dbp.id)
+	if err != nil {
+		return err
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return err
+	}
 	return nil
-}
-
-func (ps *playerService) beginTx() (*sql.Tx, error) {
-	// https://en.wikipedia.org/wiki/Isolation_(database_systems)#Isolation_levels
-	// let's use the highest level of isolation for now (which apparently uses read and write locks)
-	return ps.db.BeginTx(ps.ctx, &sql.TxOptions{Isolation: sql.LevelSerializable})
 }
