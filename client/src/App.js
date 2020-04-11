0c6275bfba9607d7fc191febed3a7574ebbcacd8
cszczepaniak <cszczepaniak@gmail.com>
2020-04-11 11:20:01 -0500

    simplify this

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
 import Landing from './components/layout/Landing';
@@ -7,11 +7,7 @@ import './App.css';
 function App() {
   return (
     <Provider store={store}>
-      <Fragment>
-        <section className='container'>
-          <Landing />
-        </section>
-      </Fragment>
+      <Landing />
     </Provider>
   );
 }
