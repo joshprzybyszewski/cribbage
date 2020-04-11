5462cba040c4553d52d24f821fe69b15ee579df1
cszczepaniak <cszczepaniak@gmail.com>
2020-04-11 11:21:09 -0500

    make this more of a landing page

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,60 +1,25 @@
-import React, { Fragment, useState } from 'react';
+import React from 'react';
+import { Button, Space } from 'antd';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { login, register } from '../../actions/auth';
 
-const Landing = ({ login, register }) => {
-  const [formData, setFormData] = useState({
-    username: '',
-    displayName: '',
-  });
-
-  const onChange = e =>
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-
-  const onSubmit = e => {
-    login(formData.username);
-  };
-
-  const handleRegister = e => {
-    register(formData.username);
-  };
-
+const Landing = props => {
   return (
-    <Fragment>
-      <h1 className='large text-primary'>Welcome to Cribbage!</h1>
-      <form className='form' onSubmit={e => onSubmit(e)}>
-        <div className='form-group'>
-          <input
-            onChange={e => onChange(e)}
-            type='text'
-            placeholder='Username'
-            name='username'
-            required
-          />
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div className='landing-inner'>
+          <h1>Welcome to Cribbage!</h1>
+          <p>Login or register to play cribbage against your friends online</p>
+          <Space>
+            <Button size='large' type='primary'>
+              Login
+            </Button>
+            <Button size='large'>Register</Button>
+          </Space>
         </div>
-        <div className='form-group'>
-          <input
-            onChange={e => onChange(e)}
-            type='text'
-            placeholder='Display Name'
-            name='displayName'
-            required
-          />
-        </div>
-
-        <button type='submit' className='btn btn-primary my-1'>
-          Login
-        </button>
-        <button
-          type='button'
-          onClick={e => handleRegister(e)}
-          className='btn my-1'
-        >
-          Register
-        </button>
-      </form>
-    </Fragment>
+      </div>
+    </section>
   );
 };
 
