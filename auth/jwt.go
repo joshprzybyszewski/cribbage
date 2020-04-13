8478950d868a03df0b56b9b77d44f6031165ceb1
cszczepaniak <cszczepaniak@gmail.com>
2020-04-12 23:04:50 -0500

    add method to validate token

--- auth/jwt.go
+++ auth/jwt.go
@@ -1,13 +1,14 @@
 package auth
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
 )
 
 type claims struct {
-	username string
+	Username string `json:"username"`
 	jwt.StandardClaims
 }
 
@@ -29,9 +30,10 @@ func NewJWTService(key string, validFor time.Duration) JWTService {
 	}
 }
 
+// CreateToken creates and signs a JSON web token
 func (js JWTService) CreateToken(username string) (Token, error) {
 	tok := jwt.NewWithClaims(jwt.SigningMethodHS256, &claims{
-		username: username,
+		Username: username,
 		StandardClaims: jwt.StandardClaims{
 			ExpiresAt: time.Now().Add(js.validFor).Unix(),
 		},
@@ -41,4 +43,25 @@ func (js JWTService) CreateToken(username string) (Token, error) {
 		return Token{}, err
 	}
 	return Token{Token: tokStr}, nil
+}
+
+// ValidateAndParseToken makes sure the token is valid given the token key, then returns the username contained within
+// the token
+func (js JWTService) ValidateAndParseToken(tokenStr string) (bool, string, error) {
+	if tokenStr == `` {
+		return false, ``, nil
+	}
+	tok, err := jwt.ParseWithClaims(tokenStr, &claims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf(`Unexpected signing method: %v`, token.Header[`alg`])
+		}
+		return js.jwtKey, nil
+	})
+	if err != nil {
+		return false, ``, err
+	}
+	if cl, ok := tok.Claims.(*claims); ok && tok.Valid {
+		return true, cl.Username, nil
+	}
+	return false, ``, nil
 }
