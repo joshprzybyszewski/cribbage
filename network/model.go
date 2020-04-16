ab10131f7c362f295c638e910a182864d428462d
cszczepaniak <cszczepaniak@gmail.com>
2020-04-16 13:54:23 -0500

    add type for creating games

--- network/model.go
+++ network/model.go
@@ -1,6 +1,12 @@
 package network
 
+import "github.com/joshprzybyszewski/cribbage/model"
+
 type CreatePlayerRequest struct {
 	Username    string `json:"username"`
 	DisplayName string `json:"displayName"`
+}
+
+type CreateGameRequest struct {
+	PlayerIDs []model.PlayerID `json:"playerIDs"`
 }
