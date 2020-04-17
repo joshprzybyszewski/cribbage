249dc05609644e7b81e10361557fe6c70adfcf2f
cszczepaniak <cszczepaniak@gmail.com>
2020-04-17 15:39:59 -0500

    be able to test multiple requests

--- server/server_test.go
+++ server/server_test.go
@@ -3,7 +3,6 @@ package server
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
@@ -30,7 +29,6 @@ func performRequest(r http.Handler, method, path string, body io.Reader) (*httpt
 
 func readBody(t *testing.T, r io.Reader, v interface{}) {
 	bs, err := ioutil.ReadAll(r)
-	fmt.Println(string(bs))
 	require.NoError(t, err)
 	err = json.Unmarshal(bs, v)
 	require.NoError(t, err)
@@ -43,46 +41,76 @@ func prepareBody(t *testing.T, v interface{}) io.Reader {
 }
 
 func TestGinPostCreatePlayer(t *testing.T) {
-	testCases := []struct {
-		msg         string
+	type testRequest struct {
 		username    string
 		displayName string
 		expCode     int
+		expErr      string
+	}
+
+	testCases := []struct {
+		msg  string
+		reqs []testRequest
 	}{{
-		msg:         `normal stuff`,
-		username:    `abc`,
-		displayName: `def`,
-		expCode:     http.StatusOK,
+		msg: `normal stuff`,
+		reqs: []testRequest{{
+			username:    `abc`,
+			displayName: `def`,
+			expCode:     http.StatusOK,
+			expErr:      ``,
+		}},
 	}, {
-		msg:         `username with weird characters shouldn't return 404`,
-		username:    `#`,
-		displayName: `#`,
-		expCode:     http.StatusBadRequest,
+		msg: `username with weird characters shouldn't return 404`,
+		reqs: []testRequest{{
+			username:    `#`,
+			displayName: `#`,
+			expCode:     http.StatusBadRequest,
+			expErr:      `Username must be alphanumeric`,
+		}},
+	}, {
+		msg: `creating the same player errors`,
+		reqs: []testRequest{{
+			username:    `abc`,
+			displayName: `def`,
+			expCode:     http.StatusOK,
+			expErr:      ``,
+		}, {
+			username:    `abc`,
+			displayName: `def`,
+			expCode:     http.StatusBadRequest,
+			expErr:      `Username already exists`,
+		}},
 	}}
 	for _, tc := range testCases {
 		// setup a new instance of the server each time to clear the db
 		cs := newCribbageServer(memory.New())
 		router := cs.NewRouter()
 
-		// make the request
-		body := prepareBody(t, network.CreatePlayerRequest{
-			Username:    model.PlayerID(tc.username),
-			DisplayName: tc.displayName,
-		})
-		w, err := performRequest(router, `POST`, `/create/player`, body)
-		require.NoError(t, err)
-
-		// verify
-		assert.Equal(t, tc.expCode, w.Code)
-		if tc.expCode == http.StatusOK {
-			expPlayer := model.Player{
-				ID:   model.PlayerID(tc.username),
-				Name: tc.displayName,
+		// make the requests
+		for _, r := range tc.reqs {
+			body := prepareBody(t, network.CreatePlayerRequest{
+				Username:    model.PlayerID(r.username),
+				DisplayName: r.displayName,
+			})
+			w, err := performRequest(router, `POST`, `/create/player`, body)
+			require.NoError(t, err)
+			// verify
+			require.Equal(t, r.expCode, w.Code)
+			if r.expCode == http.StatusOK {
+				expPlayer := model.Player{
+					ID:   model.PlayerID(r.username),
+					Name: r.displayName,
+				}
+				var player model.Player
+				readBody(t, w.Body, &player)
+				assert.NoError(t, err)
+				assert.Equal(t, expPlayer, player)
+			} else {
+				errMsgBytes, err := ioutil.ReadAll(w.Body)
+				require.NoError(t, err)
+				assert.Equal(t, r.expErr, string(errMsgBytes))
 			}
-			var player model.Player
-			readBody(t, w.Body, &player)
-			assert.NoError(t, err)
-			assert.Equal(t, expPlayer, player)
 		}
+		memory.Clear()
 	}
 }
