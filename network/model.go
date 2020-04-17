a22f488d18241d4354f59ed1557cbdef2b1b4bc5
cszczepaniak <cszczepaniak@gmail.com>
2020-04-17 15:44:43 -0500

    the network doesn't know what models are

--- network/model.go
+++ network/model.go
@@ -1,18 +1,16 @@
 package network
 
-import "github.com/joshprzybyszewski/cribbage/model"
-
 type CreatePlayerRequest struct {
-	Username    model.PlayerID `json:"username"`
-	DisplayName string         `json:"displayName"`
+	Username    string `json:"username"`
+	DisplayName string `json:"displayName"`
 }
 
 type CreateGameRequest struct {
-	PlayerIDs []model.PlayerID `json:"playerIDs"`
+	PlayerIDs []string `json:"playerIDs"`
 }
 
 type CreateInteractionRequest struct {
-	PlayerID model.PlayerID `json:"playerID"`
-	Mode     string         `json:"mode"`
-	Info     interface{}    `json:"info"`
+	PlayerID string      `json:"playerID"`
+	Mode     string      `json:"mode"`
+	Info     interface{} `json:"info"`
 }
