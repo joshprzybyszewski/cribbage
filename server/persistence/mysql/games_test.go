ed06ca013e9bdb09a60da1cba42f85cfc6cd1a96
Joshua Przybyszewski <josh.prezby@gmail.com>
2020-05-19 22:18:28 -0500

    up by one

--- server/persistence/mysql/games_test.go
+++ server/persistence/mysql/games_test.go
@@ -10,8 +10,8 @@ import (
 )
 
 func TestGetCribCards(t *testing.T) {
-	// 0x34 = decimal 52 which is more than the num cards we have
-	cards := getCribCards(int32(0x34343434))
+	// 0x35 = decimal 53 which is more than the num cards we have
+	cards := getCribCards(int32(0x35353535))
 	assert.Empty(t, cards)
 
 	cards = getCribCards(int32(0x01020304))
@@ -21,7 +21,7 @@ func TestGetCribCards(t *testing.T) {
 	assert.Equal(t, model.NewCardFromNumber(int(0x02)), cards[2])
 	assert.Equal(t, model.NewCardFromNumber(int(0x01)), cards[3])
 
-	cards = getCribCards(int32(0x01343404))
+	cards = getCribCards(int32(0x01353504))
 	require.Len(t, cards, 2)
 	assert.Equal(t, model.NewCardFromNumber(int(0x04)), cards[0])
 	assert.Equal(t, model.NewCardFromNumber(int(0x01)), cards[1])
@@ -29,7 +29,7 @@ func TestGetCribCards(t *testing.T) {
 
 func TestSerializeCribCards(t *testing.T) {
 	serVal := serializeCribCards(nil)
-	assert.Equal(t, int32(0x34343434), serVal)
+	assert.Equal(t, int32(0x35353535), serVal)
 
 	crib := []model.Card{
 		model.NewCardFromNumber(int(0x05)),
@@ -46,5 +46,5 @@ func TestSerializeCribCards(t *testing.T) {
 	}
 
 	serVal = serializeCribCards(crib)
-	assert.Equal(t, int32(0x34343408), serVal)
+	assert.Equal(t, int32(0x35353508), serVal)
 }
