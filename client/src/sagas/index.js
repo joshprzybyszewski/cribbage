534152d3a760f7b930e41ce2a3416c48fc634bab
cszczepaniak <cszczepaniak@gmail.com>
2020-05-25 10:09:36 -0500

    don't forget to add it to the root saga...

--- client/src/sagas/index.js
+++ client/src/sagas/index.js
@@ -1,6 +1,7 @@
 import { all } from 'redux-saga/effects';
 import { watchLoginAsync, watchRegisterAsync } from './auth';
+import { watchAddAlert } from './alert';
 
 export default function* rootSaga() {
-  yield all([watchLoginAsync(), watchRegisterAsync()]);
+  yield all([watchLoginAsync(), watchRegisterAsync(), watchAddAlert()]);
 }
