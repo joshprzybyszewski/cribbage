05eae635d3f32d990a6d480b452c8a08c7e71447
cszczepaniak <cszczepaniak@gmail.com>
2020-05-25 19:20:56 -0500

    rearrange stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
-import Landing from './components/layout/Landing';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Landing from './components/landing/Landing';
 import Layout from './components/layout/Layout';
 import './App.css';
 
 function App() {
   return (
     <Provider store={store}>
-      <Layout>
-        <Landing />
-      </Layout>
+      <Router>
+        <Layout>
+          <Switch>
+            <Route path='/' component={Landing} />
+            <Route path='/home' component={Home} />
+          </Switch>
+        </Layout>
+      </Router>
     </Provider>
   );
 }
