44fe1b6062ba105303ac0d04bd9aaa6560fbd1c7
cszczepaniak <cszczepaniak@gmail.com>
2020-06-30 12:29:10 -0500

    setup redux toolkit

---
+++ client/src/store/reducers.js
@@ -0,0 +1,15 @@
+import { combineReducers } from '@reduxjs/toolkit';
+import { connectRouter } from 'connected-react-router';
+import { createBrowserHistory } from 'history';
+
+const history = createBrowserHistory();
+
+export const createReducer = injectedReducers => {
+  if (Object.keys(injectedReducers).length === 0) {
+    return state => state;
+  }
+  return combineReducers({
+    router: connectRouter(history),
+    ...injectedReducers,
+  });
+};
