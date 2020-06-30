cde2848749b2c5114dcfa30ac3e2e676429d9dcf
cszczepaniak <cszczepaniak@gmail.com>
2020-06-30 16:10:08 -0500

    a lil refactoring

--- client/src/app/containers/Login/index.jsx
+++ client/src/app/containers/Login/index.jsx
@@ -13,20 +13,18 @@ const LoginForm = ({ login }) => {
     event.preventDefault();
     dispatch(actions.login(playerID));
   };
+  const onUserIDInputChange = event => {
+    setPlayerID(event.target.value);
+  };
 
   const [playerID, setPlayerID] = useState('');
   return (
     <div className='max-w-sm m-auto mt-12'>
       <h1 className='text-4xl'>Login to Cribbage</h1>
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-          login(playerID);
-        }}
-      >
+      <form onSubmit={onLoginFormSubmit}>
         <input
           placeholder='Username'
-          onChange={e => setPlayerID(e.target.value)}
+          onChange={onUserIDInputChange}
           className='form-input'
         ></input>
         <p className='mt-1 text-xs text-gray-600'>
