37a3852760cbf1ec337bfb6c380db561093c999b
joshprzybyszewski <josh.prezby@gmail.com>
2020-07-05 11:00:45 -0500

    start making the game page readable

--- client/src/app/containers/Game/index.jsx
+++ client/src/app/containers/Game/index.jsx
@@ -19,27 +19,107 @@ const Game = () => {
   const onRefreshCurrentGame = () => {
     dispatch(actions.refreshGame(activeGameID, history));
   };
+  const refreshButton = <button
+    onClick={onRefreshCurrentGame}
+    className='hover:text-white'
+    >
+    Refresh
+  </button>;
+
+  if ( !activeGame ) {
+    return <div>
+      This will be a page for a game, but we don't know what the game is.
+      <br></br>
+      {refreshButton}
+    </div>;
+  }
 
   let gameResp = [];
-  if ( activeGame ) {
-    for (const [key, val] of Object.entries(activeGame)) {
-        gameResp.push(`${key}: ${val} `);
-        gameResp.push(<br key={`br ${key}`}></br>);
+  let gameDesc = 'Players are: ';
+  let scoreChildren = [];
+  let dealerDesc = 'Dealer: ';
+  let phaseDesc = 'Phase: ';
+  let cutCardDiv;
+  for (const [key, val] of Object.entries(activeGame)) {
+    gameResp.push(`${key}: ${val} `);
+    gameResp.push(<br key={`br ${key}`}></br>);
+
+    switch (key) {
+        case 'players':
+            val.forEach((player, index) => {
+                gameDesc += player.name;
+                if ( index < val.length - 1 ) {
+                    gameDesc += ', ';
+                }
+            });
+            break;
+        case 'current_scores':
+            let lagScores = activeGame['lag_scores'];
+            let playerColors = activeGame['player_colors'];
+            let teams = {};
+            for (const [playerName, color] of Object.entries(playerColors)) {
+                if ( teams[color] ) {
+                    teams[color] += `, ${playerName}`
+                } else {
+                    teams[color] = `${playerName}`
+                }
+            }
+            for (const [color, curscore] of Object.entries(val)) {
+                let team = ` (${teams[color]})`;
+                let scoreStr = `${color}${team}: ${curscore}`;
+                if (lagScores && lagScores[color]) {
+                    scoreStr += ` (from ${lagScores[color]})`
+                }
+                scoreChildren.push(scoreStr);
+                scoreChildren.push(<br key={`br ${color}`}></br>);
+            }
+            break;
+        case 'current_dealer':
+            dealerDesc += val;
+            break;
+        case 'phase':
+            phaseDesc += val;
+            break;
+        case 'cut_card':
+            let skip = false;
+            switch (activeGame['phase']) {
+                case 'Deal':
+                case 'BuildCrib':
+                    skip = true;
+                    break;
+            }
+            if ( skip ) {
+                break;
+            }
+            cutCardDiv = <div key={'cutCardDiv'}>
+                Cut Card: {val.name} ({val.value} of {val.suit})
+            </div>;
+            break;
     }
   }
 
-
   return <div>
+      <div key={'gameDescDiv'}>
+        {gameDesc}
+      </div>
+      <div key={'scoresDiv'}>
+          <h2>Scores:</h2>
+        {scoreChildren}
+      </div>
+      <div key={'dealerDiv'}>
+        {dealerDesc}
+      </div>
+      <div key={'phaseDiv'}>
+        {phaseDesc}
+      </div>
+      {cutCardDiv}
+
+      <br></br>
       This will be a page for the game of a user.
       <br></br>
       {gameResp}
       <br></br>
-      <button
-          onClick={onRefreshCurrentGame}
-          className='hover:text-white'
-        >
-          Refresh
-        </button>
+      {refreshButton}
     </div>;
 };
 
