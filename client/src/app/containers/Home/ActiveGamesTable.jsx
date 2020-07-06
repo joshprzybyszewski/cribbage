a5c94285e657c9d02cd66365acfec73cf5941c5c
joshprzybyszewski <josh.prezby@gmail.com>
2020-07-06 11:10:44 -0500

    make a table for active games

--- client/src/app/containers/Home/ActiveGamesTable.jsx
+++ client/src/app/containers/Home/ActiveGamesTable.jsx
@@ -28,26 +28,75 @@ const ActiveGamesTable = () => {
         if ( !gID || !desc ) {
             continue;
         }
-        gameButtons.push(<br key={`br ${gID}`}></br>)
-        gameButtons.push(<button
-            key={gID}
-            onClick={() => goToGame({gID})}
-        >
-        {desc}
-        </button>);
+
+gameButtons.push(
+<tr>
+<td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+  {desc}
+</td>
+<td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
+  (not implemented)
+</td>
+<td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
+  (not implemented)
+</td>
+<td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
+(not implemented)
+</td>
+<td class="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
+  <button
+    key={gID}
+    onClick={() => goToGame({gID})}
+  >
+    Play!
+  </button>
+</td>
+</tr>
+);
     }
   }
 
   return <div>
-      This is supposed to be {currentUser.name}'s Active Games page.
-      {gameButtons}
-      <br></br>
-      <button
-          onClick={onRefreshActiveGames}
-          className='hover:text-white'
-        >
-          Refresh
-        </button>
+
+        <div class="flex flex-col">
+  <div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+    <div class="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200">
+      <table class="min-w-full">
+        <thead>
+          <tr>
+            <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+              Opponents
+            </th>
+            <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+              Your Color
+            </th>
+            <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+              Created
+            </th>
+            <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+              Last Move
+            </th>
+            <th class="px-6 py-3 border-b border-gray-200 bg-gray-50">
+            <div
+            class="flex-shrink-0 h-10 w-10"
+            onClick={onRefreshActiveGames}
+            >
+                  <img
+                    class="h-10 w-10 rounded-full"
+                    src="./refresh.png"
+                    alt="Refresh"
+                     />
+                </div>
+            </th>
+          </tr>
+        </thead>
+        <tbody class="bg-white">
+          {gameButtons}
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
     </div>;
 };
 
