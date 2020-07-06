7ce495756714d61382059f0b87b001f315568d89
joshprzybyszewski <josh.prezby@gmail.com>
2020-07-06 14:49:49 -0500

    prettier

--- client/src/app/containers/Home/ActiveGamesTable.jsx
+++ client/src/app/containers/Home/ActiveGamesTable.jsx
@@ -6,7 +6,11 @@ import { authSaga } from '../../../auth/saga';
 import { sliceKey, reducer, actions } from '../../../auth/slice';
 import { selectActiveGames } from '../../../home/selectors';
 import { homeSaga } from '../../../home/saga';
-import { sliceKey as homeSliceKey, reducer as homeReducer, actions as homeActions } from '../../../home/slice';
+import {
+  sliceKey as homeSliceKey,
+  reducer as homeReducer,
+  actions as homeActions,
+} from '../../../home/slice';
 
 const ActiveGamesTable = () => {
   useInjectReducer({ key: sliceKey, reducer: reducer });
@@ -16,93 +20,89 @@ const ActiveGamesTable = () => {
   const dispatch = useDispatch();
   const currentUser = useSelector(selectCurrentUser);
   const activeGames = useSelector(selectActiveGames(currentUser.id));
-  
+
   // event handlers
   const onRefreshActiveGames = () => {
     dispatch(homeActions.refreshActiveGames({ id: currentUser.id }));
   };
 
   let gameButtons = [];
-  if ( activeGames ) {
+  if (activeGames) {
     for (const [gID, desc] of Object.entries(activeGames)) {
-        if ( !gID || !desc ) {
-            continue;
-        }
+      if (!gID || !desc) {
+        continue;
+      }
 
-gameButtons.push(
-<tr>
-<td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-  {desc}
-</td>
-<td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
-  (not implemented)
-</td>
-<td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
-  (not implemented)
-</td>
-<td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
-(not implemented)
-</td>
-<td class="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
-  <button
-    key={gID}
-    onClick={() => goToGame({gID})}
-  >
-    Play!
-  </button>
-</td>
-</tr>
-);
+      gameButtons.push(
+        <tr>
+          <td class='px-6 py-4 whitespace-no-wrap border-b border-gray-200'>
+            {desc}
+          </td>
+          <td class='px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500'>
+            (not implemented)
+          </td>
+          <td class='px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500'>
+            (not implemented)
+          </td>
+          <td class='px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500'>
+            (not implemented)
+          </td>
+          <td class='px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium'>
+            <button key={gID} onClick={() => goToGame({ gID })}>
+              Play!
+            </button>
+          </td>
+        </tr>,
+      );
     }
   }
 
-  return <div>
-
-        <div class="flex flex-col">
-  <div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
-    <div class="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200">
-      <table class="min-w-full">
-        <thead>
-          <tr>
-            <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-              Opponents
-            </th>
-            <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-              Your Color
-            </th>
-            <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-              Created
-            </th>
-            <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-              Last Move
-            </th>
-            <th class="px-6 py-3 border-b border-gray-200 bg-gray-50">
-            <div
-            class="flex-shrink-0 h-10 w-10"
-            onClick={onRefreshActiveGames}
-            >
-                  <img
-                    class="h-10 w-10 rounded-full"
-                    src="./refresh.png"
-                    alt="Refresh"
-                     />
-                </div>
-            </th>
-          </tr>
-        </thead>
-        <tbody class="bg-white">
-          {gameButtons}
-        </tbody>
-      </table>
+  return (
+    <div>
+      <div class='flex flex-col'>
+        <div class='-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8'>
+          <div class='align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200'>
+            <table class='min-w-full'>
+              <thead>
+                <tr>
+                  <th class='px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider'>
+                    Opponents
+                  </th>
+                  <th class='px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider'>
+                    Your Color
+                  </th>
+                  <th class='px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider'>
+                    Created
+                  </th>
+                  <th class='px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider'>
+                    Last Move
+                  </th>
+                  <th class='px-6 py-3 border-b border-gray-200 bg-gray-50'>
+                    <div
+                      class='flex-shrink-0 h-10 w-10'
+                      onClick={onRefreshActiveGames}
+                    >
+                      <img
+                        class='h-10 w-10 rounded-full'
+                        src='./refresh.png'
+                        alt='Refresh'
+                      />
+                    </div>
+                  </th>
+                </tr>
+              </thead>
+              <tbody class='bg-white'>{gameButtons}</tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-    </div>;
+  );
 };
 
-const goToGame = (gID) => {
-    // We're gonna need to navigate to the games page
-    console.log(`Requesting game page for ID: ${gID}`)
-}
+const goToGame = gID => {
+  // We're gonna need to navigate to the games page
+  console.log(`Requesting game page for ID: ${gID}`);
+};
 
 export default ActiveGamesTable;
--- client/src/home/selectors.js
+++ client/src/home/selectors.js
@@ -3,7 +3,7 @@ import { initialState } from './slice';
 
 const selectDomain = state => state.home || initialState;
 
-export const selectActiveGames = (playerID) => createSelector(
-  [selectDomain],
-  homeState => homeState.activeGamesPlayerID === playerID ? homeState.activeGames : {},
-);
+export const selectActiveGames = playerID =>
+  createSelector([selectDomain], homeState =>
+    homeState.activeGamesPlayerID === playerID ? homeState.activeGames : {},
+  );
