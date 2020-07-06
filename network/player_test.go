c4c449d3fd24e51b03140d73161a71e044459801
joshprzybyszewski <josh.prezby@gmail.com>
2020-07-06 15:13:27 -0500

    update test

--- network/player_test.go
+++ network/player_test.go
@@ -2,6 +2,7 @@ package network
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 
@@ -66,6 +67,13 @@ func TestConvertToCreatePlayerResponse(t *testing.T) {
 }
 
 func TestConvertToGetActiveGamesForPlayerResponse(t *testing.T) {
+	aliceID := model.PlayerID(`alice`)
+	bobID := model.PlayerID(`bob`)
+	chelseaID := model.PlayerID(`chelsea`)
+	daveID := model.PlayerID(`dave`)
+	erinID := model.PlayerID(`erin`)
+	francisID := model.PlayerID(`francis`)
+
 	tests := []struct {
 		desc       string
 		player     model.Player
@@ -74,8 +82,8 @@ func TestConvertToGetActiveGamesForPlayerResponse(t *testing.T) {
 	}{{
 		desc: ``,
 		player: model.Player{
-			ID:   `a`,
-			Name: `aa`,
+			ID:   aliceID,
+			Name: `alice`,
 			Games: map[model.GameID]model.PlayerColor{
 				123: model.Blue,
 				456: model.Red,
@@ -85,35 +93,86 @@ func TestConvertToGetActiveGamesForPlayerResponse(t *testing.T) {
 		inputGames: map[model.GameID]model.Game{
 			123: {
 				Players: []model.Player{{
+					ID:   aliceID,
 					Name: `alice`,
 				}, {
+					ID:   bobID,
 					Name: `bob`,
 				}},
+				PlayerColors: map[model.PlayerID]model.PlayerColor{
+					aliceID: model.Red,
+					bobID:   model.Blue,
+				},
 			},
 			456: {
 				Players: []model.Player{{
+					ID:   chelseaID,
 					Name: `chelsea`,
 				}, {
+					ID:   daveID,
 					Name: `dave`,
 				}},
+				PlayerColors: map[model.PlayerID]model.PlayerColor{
+					chelseaID: model.Red,
+					daveID:    model.Blue,
+				},
 			},
 			789: {
 				Players: []model.Player{{
+					ID:   erinID,
 					Name: `erin`,
 				}, {
+					ID:   francisID,
 					Name: `francis`,
 				}},
+				PlayerColors: map[model.PlayerID]model.PlayerColor{
+					erinID:    model.Red,
+					francisID: model.Blue,
+				},
 			},
 		},
 		expResp: GetActiveGamesForPlayerResponse{
 			Player: Player{
-				ID:   `a`,
-				Name: `aa`,
+				ID:   aliceID,
+				Name: `alice`,
 			},
-			ActiveGames: map[model.GameID]string{
-				123: `alice, bob`,
-				456: `chelsea, dave`,
-				789: `erin, francis`,
+			ActiveGames: map[model.GameID]ActiveGame{
+				123: ActiveGame{
+					PlayerNamesByID: map[model.PlayerID]string{
+						aliceID: `alice`,
+						bobID:   `bob`,
+					},
+					PlayerColorsByID: map[model.PlayerID]string{
+						aliceID: `red`,
+						bobID:   `blue`,
+					},
+					Created:  time.Time{},
+					LastMove: time.Time{},
+				},
+				456: ActiveGame{
+					PlayerNamesByID: map[model.PlayerID]string{
+						chelseaID: `chelsea`,
+						daveID:    `dave`,
+					},
+					PlayerColorsByID: map[model.PlayerID]string{
+						chelseaID: `red`,
+						daveID:    `blue`,
+					},
+					Created:  time.Time{},
+					LastMove: time.Time{},
+				},
+				789: ActiveGame{
+					PlayerNamesByID: map[model.PlayerID]string{
+						erinID:    `erin`,
+						francisID: `francis`,
+					},
+					PlayerColorsByID: map[model.PlayerID]string{
+						erinID:    `red`,
+						francisID: `blue`,
+					},
+					Created:  time.Time{},
+					LastMove: time.Time{},
+				},
 			},
 		},
 	}}
