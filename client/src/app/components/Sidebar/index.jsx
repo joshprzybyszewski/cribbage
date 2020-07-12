ed4bd193b98446fbf7bc56c08fb203332a5eeb76
cszczepaniak <cszczepaniak@gmail.com>
2020-07-12 18:35:50 -0500

    switch from tabler to material

--- client/src/app/components/Sidebar/index.jsx
+++ client/src/app/components/Sidebar/index.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import * as Icons from '../icons';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import HomeIcon from '@material-ui/icons/Home';
+import PersonIcon from '@material-ui/icons/Person';
 
 export default () => {
   return (
     <div className='w-12 bg-gray-400'>
       <div className='flex flex-col py-4 items-center'>
-        <Link to='/newgame'>
-          <Icons.CirclePlusIcon className='mb-4 w-8 text-blue-800 hover:text-blue-600' />
-        </Link>
         <Link to='/home'>
-          <Icons.ListIcon className='mb-4 w-8 text-blue-800 hover:text-blue-600' />
+          <HomeIcon
+            fontSize='large'
+            className='mb-4 text-blue-800 hover:text-blue-600'
+          />
+        </Link>
+        <Link to='/newgame'>
+          <AddCircleOutlineIcon
+            fontSize='large'
+            className='mb-4 text-blue-800 hover:text-blue-600'
+          />
         </Link>
         <Link to='/account'>
-          <Icons.UserIcon className='w-8 text-blue-800 hover:text-blue-600' />
+          <PersonIcon
+            fontSize='large'
+            className='text-blue-800 hover:text-blue-600'
+          />
         </Link>
       </div>
     </div>
