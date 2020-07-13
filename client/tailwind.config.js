e2606785e398727ea224ab1a0e4b0d2ae01150e1
cszczepaniak <cszczepaniak@gmail.com>
2020-07-12 19:24:41 -0500

    makefile should watch tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   purge: ['./src/**/components/*.js'],
   theme: {
-    extend: {},
+    flex: {
+      '1': '1 1 0%',
+      auto: '1 1 auto',
+      inherit: 'inherit',
+      none: 'none',
+      '2': '2 2 0%',
+      '3': '3 3 0%',
+    },
   },
   variants: {},
   plugins: [],
