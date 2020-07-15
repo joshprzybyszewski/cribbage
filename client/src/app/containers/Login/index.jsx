3c96d29a6c2f77a2d48dcb925fe50296fdb8e663
cszczepaniak <cszczepaniak@gmail.com>
2020-07-14 19:46:10 -0500

    use material ui on login and register

--- client/src/app/containers/Login/index.jsx
+++ client/src/app/containers/Login/index.jsx
@@ -1,9 +1,37 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useInjectReducer, useInjectSaga } from 'redux-injectors';
-import { Link, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { authSaga } from '../../../auth/saga';
 import { sliceKey, reducer, actions } from '../../../auth/slice';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  Button,
+  Container,
+  Link,
+  TextField,
+  CssBaseline,
+  Typography,
+} from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  title: {
+    fontSize: '2rem',
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 const LoginForm = () => {
   // hooks
@@ -22,33 +50,68 @@ const LoginForm = () => {
     setPlayerID(event.target.value);
   };
 
+  const classes = useStyles();
+
   return (
-    <div className='max-w-sm m-auto mt-12'>
-      <h1 className='text-4xl'>Login to Cribbage</h1>
-      <form onSubmit={onSubmitLoginForm}>
-        <input
-          placeholder='Username'
-          onChange={onInputChange}
-          className='form-input'
-        ></input>
-        <p className='mt-1 text-xs text-gray-600'>
-          Don't have an account?{' '}
-          <span>
-            <Link
-              to='/register'
-              className='hover:text-gray-500 hover:underline'
-            >
-              Register here.
-            </Link>
-          </span>
-        </p>
-        <input
-          type='submit'
-          value='login'
-          className='mt-1 btn btn-primary'
-        ></input>
-      </form>
-    </div>
+    <Container component='main' maxWidth='sm'>
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Typography component='h1' className={classes.title}>
+          Welcome to Cribbage!
+        </Typography>
+        <p>Play cribbage against your friends online. Get started now!</p>
+        <form className={classes.form} onSubmit={onSubmitLoginForm}>
+          <TextField
+            variant='outlined'
+            margin='normal'
+            required
+            fullWidth
+            label='Username'
+            name='id'
+            autoFocus
+            onChange={onInputChange}
+          />
+          <Button
+            type='submit'
+            fullWidth
+            variant='contained'
+            color='primary'
+            className={classes.submit}
+          >
+            Login
+          </Button>
+        </form>
+        <Link href='/register' variant='body2'>
+          Don't have an account? Register here
+        </Link>
+      </div>
+    </Container>
+    // <div className='max-w-sm m-auto mt-12'>
+    //   <h1 className='text-4xl'>Login to Cribbage</h1>
+    //   <form onSubmit={onSubmitLoginForm}>
+    //     <input
+    //       placeholder='Username'
+    //       onChange={onInputChange}
+    //       className='form-input'
+    //     ></input>
+    //     <p className='mt-1 text-xs text-gray-600'>
+    //       Don't have an account?{' '}
+    //       <span>
+    //         <Link
+    //           to='/register'
+    //           className='hover:text-gray-500 hover:underline'
+    //         >
+    //           Register here.
+    //         </Link>
+    //       </span>
+    //     </p>
+    //     <input
+    //       type='submit'
+    //       value='login'
+    //       className='mt-1 btn btn-primary'
+    //     ></input>
+    //   </form>
+    // </div>
   );
 };
 
--- client/src/app/containers/Register/index.jsx
+++ client/src/app/containers/Register/index.jsx
@@ -1,11 +1,39 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useInjectReducer, useInjectSaga } from 'redux-injectors';
-import { Link, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { authSaga } from '../../../auth/saga';
 import { sliceKey, reducer, actions } from '../../../auth/slice';
+import {
+  Button,
+  Container,
+  Link,
+  TextField,
+  CssBaseline,
+  Typography,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-const Landing = () => {
+const useStyles = makeStyles(theme => ({
+  title: {
+    fontSize: '2rem',
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+const RegisterForm = () => {
   // hooks
   useInjectReducer({ key: sliceKey, reducer: reducer });
   useInjectSaga({ key: sliceKey, saga: authSaga });
@@ -21,43 +49,84 @@ const Landing = () => {
   const onInputChange = event =>
     setFormData({ ...formData, [event.target.name]: event.target.value });
 
+  const classes = useStyles();
+
   return (
-    <div className='max-w-sm m-auto mt-12'>
-      <h1 className='text-4xl'>Welcome to Cribbage!</h1>
-      <p>Play cribbage against your friends online. Get started now!</p>
-      <form onSubmit={onSubmitForm}>
-        <input
-          name='id'
-          onChange={onInputChange}
-          value={formData.id}
-          placeholder='Username'
-          required
-          className='mt-2 form-input'
-        ></input>
-        <input
-          name='name'
-          onChange={onInputChange}
-          value={formData.name}
-          placeholder='Display name'
-          required
-          className='mt-2 form-input'
-        ></input>
-        <p className='mt-1 text-xs text-gray-600'>
-          Already have an account?{' '}
-          <span>
-            <Link to='/' className='hover:text-gray-500 hover:underline'>
-              Log in here.
-            </Link>
-          </span>
-        </p>
-        <input
-          type='submit'
-          value='register'
-          className='mt-1 btn btn-primary'
-        ></input>
-      </form>
-    </div>
+    <Container component='main' maxWidth='sm'>
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Typography component='h1' className={classes.title}>
+          Welcome to Cribbage!
+        </Typography>
+        <p>Play cribbage against your friends online. Get started now!</p>
+        <form className={classes.form} onSubmit={onSubmitForm}>
+          <TextField
+            variant='outlined'
+            margin='normal'
+            required
+            fullWidth
+            label='Username'
+            name='id'
+            autoFocus
+            onChange={onInputChange}
+          />
+          <TextField
+            variant='outlined'
+            margin='normal'
+            required
+            fullWidth
+            name='name'
+            label='Display Name'
+            onChange={onInputChange}
+          />
+          <Button
+            type='submit'
+            fullWidth
+            variant='contained'
+            color='primary'
+            className={classes.submit}
+          >
+            Register
+          </Button>
+        </form>
+        <Link href='/' variant='body2'>
+          Already have an account? Login here
+        </Link>
+      </div>
+    </Container>
+    // <div className='max-w-sm m-auto mt-12'>
+    //     <input
+    //       name='id'
+    //       onChange={onInputChange}
+    //       value={formData.id}
+    //       placeholder='Username'
+    //       required
+    //       className='mt-2 form-input'
+    //     ></input>
+    //     <input
+    //       name='name'
+    //       onChange={onInputChange}
+    //       value={formData.name}
+    //       placeholder='Display name'
+    //       required
+    //       className='mt-2 form-input'
+    //     ></input>
+    //     <p className='mt-1 text-xs text-gray-600'>
+    //       Already have an account?{' '}
+    //       <span>
+    //         <Link to='/' className='hover:text-gray-500 hover:underline'>
+    //           Log in here.
+    //         </Link>
+    //       </span>
+    //     </p>
+    //     <input
+    //       type='submit'
+    //       value='register'
+    //       className='mt-1 btn btn-primary'
+    //     ></input>
+    //   </form>
+    // </div>
   );
 };
 
-export default Landing;
+export default RegisterForm;
