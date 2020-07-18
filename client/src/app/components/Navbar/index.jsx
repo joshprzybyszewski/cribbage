b891bfee2121191f649a06f766800a73b8eed969
cszczepaniak <cszczepaniak@gmail.com>
2020-07-18 11:21:19 -0500

    rm unused stuff

--- client/src/app/components/Navbar/index.jsx
+++ client/src/app/components/Navbar/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import AppBar from '@material-ui/core/AppBar';
-import grey from '@material-ui/core/colors/grey';
 import IconButton from '@material-ui/core/IconButton';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -10,14 +9,6 @@ import MenuIcon from '@material-ui/icons/Menu';
 import PropTypes from 'prop-types';
 
 const useStyles = makeStyles(theme => ({
-  loggedOutLink: {
-    color: grey[200],
-    marginRight: theme.spacing(2),
-  },
-  logoutButton: {
-    color: grey[200],
-    textTransform: 'capitalize',
-  },
   menuButton: {
     marginRight: theme.spacing(2),
   },
