e683b1290950ddd22c2eab98f88ad37679594346
joshprzybyszewski <josh.prezby@gmail.com>
2020-07-19 11:07:27 -0500

    pass along isDisabled
    gotta be a better way to do this tho

--- client/src/app/containers/Game/ActionBox.jsx
+++ client/src/app/containers/Game/ActionBox.jsx
@@ -5,12 +5,12 @@ import Grid from '@material-ui/core/Grid';
 
 import { gameSaga } from './saga';
 import { sliceKey, reducer } from './slice';
-import { DealAction } from './DealAction';
-import { CribAction } from './CribAction';
-import { CutAction } from './CutAction';
-import { PegAction } from './PegAction';
-import { CountHandAction } from './CountHandAction';
-import { CountCribAction } from './CountCribAction';
+import DealAction from './DealAction';
+import CribAction from './CribAction';
+import CutAction from './CutAction';
+import PegAction from './PegAction';
+import CountHandAction from './CountHandAction';
+import CountCribAction from './CountCribAction';
 
 const ActionBox = props => {
   useInjectReducer({ key: sliceKey, reducer: reducer });
@@ -19,17 +19,17 @@ const ActionBox = props => {
   return (
     <Grid item container justify='center' spacing={1}>
       {props.phase === 'Deal' ? (
-        <DealAction />
+        <DealAction isBlocking={props.isBlocking} />
       ) : props.phase === 'BuildCrib' ? (
-        <CribAction />
+        <CribAction isBlocking={props.isBlocking} />
       ) : props.phase === 'Cut' ? (
-        <CutAction />
+        <CutAction isBlocking={props.isBlocking} />
       ) : props.phase === 'Pegging' ? (
-        <PegAction />
+        <PegAction isBlocking={props.isBlocking} />
       ) : props.phase === 'Counting' ? (
-        <CountHandAction />
+        <CountHandAction isBlocking={props.isBlocking} />
       ) : props.phase === 'CribCounting' ? (
-        <CountCribAction />
+        <CountCribAction isBlocking={props.isBlocking} />
       ) : (
         'dev error!'
       )}
