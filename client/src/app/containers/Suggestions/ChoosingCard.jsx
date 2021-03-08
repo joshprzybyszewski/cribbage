9cdbfe06e69225854d2bd06411f8eaf39961703a
Josh Przybyszewski <josh.prezby@gmail.com>
2021-03-07 18:41:25 -0600

    poke around at making the choosing card choosy

--- client/src/app/containers/Suggestions/ChoosingCard.jsx
+++ client/src/app/containers/Suggestions/ChoosingCard.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import { makeStyles } from '@material-ui/core/styles';
+import Slider from '@material-ui/core/Slider';
 import Typography from '@material-ui/core/Typography';
+import Tooltip from '@material-ui/core/Tooltip';
 import { sliceKey, reducer } from 'app/containers/Suggestions/slice';
 import PropTypes from 'prop-types';
 import { useInjectReducer } from 'redux-injectors';
@@ -12,6 +14,7 @@ const useStyles = makeStyles({
   root: {
     width: 120,
     height: 160,
+    // display: 'flex',
   },
   value: {
     fontSize: 14,
@@ -22,83 +25,169 @@ const useStyles = makeStyles({
     verticalAlign: 'center',
     textAlign: 'center',
   },
+  // fauxCardWrapper: {
+  //   display: 'flex',
+  //   flexDirection: 'column',
+  // },
+  // fauxCard: {
+  //   flex: '1 0 auto',
+  // },
+  valueSlider: {
+    // width: '5%',
+    // height: '100%',
+    width: '100%',
+    height: '100%',
+    position: 'relative',
+    top: '0',
+    left: '0',
+  }
 });
 
-function getSuit(card) {
-  if (card.includes('C') || card.includes('c')) {
-    return 'Clubs';
-  } else if (card.includes('S') || card.includes('S')) {
-    return 'Spades';
-  } else if (card.includes('H') || card.includes('h')) {
-    return 'Hearts';
-  } else if (card.includes('D') || card.includes('d')) {
-    return 'Diamonds';
-  } 
+function getSuitValue(card) {
+  if (card.includes('C')) {
+    return 2;
+  } else if (card.includes('S')) {
+    return 1;
+  } else if (card.includes('H')) {
+    return 4;
+  } else if (card.includes('D')) {
+    return 3;
+  }
 
-  return 'Unknown'
+  return 0;
+}
+
+function getSuitEmoji(card) {
+  if (card.includes('C')) {
+    return '♣️';
+  } else if (card.includes('S')) {
+    return '♠️';
+  } else if (card.includes('H')) {
+    return '♥️';
+  } else if (card.includes('D')) {
+    return '♦️';
+  }
+
+  return '?';
 }
 
 function getValue(card) {
-  if (card.startsWith('K') || card.startsWith('k') || card.startsWith('13')) {
-    return 'K';
-  } else if (card.startsWith('Q') || card.startsWith('q') || card.startsWith('12')) {
-    return 'Q';
-  } else if (card.startsWith('J') || card.startsWith('j') || card.startsWith('11')) {
-    return 'J';
+  if (card.startsWith('K') || card.startsWith('13')) {
+    return 13; // 'K';
+  } else if (card.startsWith('Q') || card.startsWith('12')) {
+    return 12; // 'Q';
+  } else if (card.startsWith('J') || card.startsWith('11')) {
+    return 11; // 'J';
   } else if (card.startsWith('10')) {
-    return '10';
-  } else if (card.startsWith('A') || card.startsWith('a') || card.startsWith('1')) {
+    return 10; // '10';
+  } else if (card.startsWith('A') || card.startsWith('1')) {
+    return 1; // 'A';
+  }
+
+  return parseInt(card.substr(0, card.length - 1));
+}
+
+function getValueString(val) {
+  if (val === 13) {
+    return 'K';
+  } else if (val === 12) {
+    return 'Q';
+  } else if (val === 11) {
+    return 'J';
+  } else if (val === 1) {
     return 'A';
   }
 
-  return card.substr(0, card.length-1)
+  return `${val}`;
+}
+
+function ValueLabelComponent(props) {
+  const { children, open, value } = props;
+
+  return (
+    <Tooltip open={open} enterTouchDelay={0} placement="top" title={getValueString(value)}>
+      {children}
+    </Tooltip>
+  );
 }
 
 const ChoosingCard = ({ card, notEditable }) => {
   useInjectReducer({ key: sliceKey, reducer: reducer });
+
   const classes = useStyles();
+  const value = getValue(card.toUpperCase());
+  const [curVal, setCurVal] = useState(value);
 
-  const suit = getSuit(card);
-  const value = getValue(card);
-  const useRed = !['Spades', 'Clubs'].includes(suit);
+  const suitVal = getSuitValue(card.toUpperCase());
+  const [curSuit, setCurSuit] = useState(suitVal);
 
-  const updateValue=(_) => {
-    // TODO figure out how to get scroll capturing to work
-    // TODO update the value of this card in the state
-    console.log(`scrolled: ${card}`);
-  };
-  const updateSuit=(_) => {
+
+  const useRed = card.toUpperCase().includes('H') || card.toUpperCase().includes('D');
+
+  const updateValue = () => {
     // TODO update the state so that this card increments suits
-    console.log(`clicked: ${card}`);
+    console.log(`updateValue: ${notEditable} ${card}, ${curVal}`);
+    setCurVal(prev => (prev % 13) + 1);
+  }
+  const updateSuit = (_) => {
+    // TODO update the state so that this card increments suits
+    console.log(`updateSuit: ${notEditable} ${card}, ${curSuit}`);
+    setCurSuit(prev => (prev % 4) + 1);
   };
 
-    return (
-      <Card 
-      onScroll={updateValue}
-      onClick={updateSuit}
+  return (
+    <Card
       className={classes.root}>
-        <CardContent>
+      <div
+        className={classes.fauxCardWrapper}
+      >
+        <CardContent
+        className={classes.fauxCard}
+          onClick={updateSuit}
+        >
           <Typography
             className={classes.value}
             color={useRed ? 'red' : 'black'}
             gutterBottom
           >
-            {value}
+            {getValueString(value)}
           </Typography>
           <Typography className={classes.suit}>
-            {suit === 'Spades'
-              ? '♠️'
-              : suit === 'Clubs'
-              ? '♣️'
-              : suit === 'Diamonds'
-              ? '♦️'
-              : suit === 'Hearts'
-              ? '♥️'
-              : '?'}
+            {getSuitEmoji(card)}
           </Typography>
         </CardContent>
-      </Card>
-    );
+      {
+        !notEditable &&
+        <div
+          className={classes.valueSlider}
+        >
+          <CardContent>
+            <Slider
+              orientation='vertical'
+              defaultValue={curVal}
+              ValueLabelComponent={ValueLabelComponent}
+              getAriaValueText={v => {
+                console.log(`getAriaValueText: ${v}`);
+                getValueString(v);
+              }
+              }
+              aria-labelledby="discrete-slider"
+              valueLabelDisplay="auto"
+              step={1}
+              marks
+              min={1}
+              max={13}
+              onChange={event => {
+                console.log(`onChange: ${event.target.value}`);
+                // dispatch(actions.claimPoints(Number(event.target.value) / 100));
+              }}
+            />
+          </CardContent>
+        </div>
+      }
+      </div>
+    </Card>
+  );
 };
 
 ChoosingCard.propTypes = {
