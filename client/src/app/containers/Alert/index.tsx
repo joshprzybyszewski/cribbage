9b9e9add10fd1a8f41da46d46ad03897c7025c5b
Connor Szczepaniak <cszczepaniak@gmail.com>
2021-03-19 18:36:30 -0500

    remove tailwind from alert

--- client/src/app/containers/Alert/index.tsx
+++ client/src/app/containers/Alert/index.tsx
@@ -1,19 +1,60 @@
 import React from 'react';
 
+import { makeStyles, Typography } from '@material-ui/core';
+import { green, grey, red, yellow } from '@material-ui/core/colors';
+import clsx from 'clsx';
 import { useSelector } from 'react-redux';
 
 import { RootState } from '../../../store/store';
 
+const useStyles = makeStyles(theme => ({
+    container: {
+        position: 'fixed',
+        width: '50vw',
+        right: 0,
+        padding: theme.spacing(0.25, 0.5),
+        zIndex: 100,
+    },
+    alert: {
+        // flex items-center h-10 px-4 rounded-lg border-solid border-2;
+        display: 'flex',
+        alignItems: 'center',
+        padding: theme.spacing(0, 0.5),
+        marginBottom: theme.spacing(0.25),
+        borderStyle: 'solid',
+        borderWidth: 2,
+        borderRadius: 4,
+    },
+    success: {
+        backgroundColor: green[200],
+        borderColor: green[300],
+    },
+    error: {
+        backgroundColor: red[200],
+        borderColor: red[300],
+    },
+    warning: {
+        backgroundColor: yellow[200],
+        borderColor: yellow[300],
+    },
+    info: {
+        backgroundColor: grey[400],
+        borderColor: grey[500],
+    },
+}));
+
 const Alert: React.FunctionComponent = () => {
     const alerts = useSelector((state: RootState) => state.alerts);
-
+    const classes = useStyles();
     return (
-        // TODO get rid of tailwind!
-        <div className='fixed w-half-screen right-0 px-3 py-2 z-50'>
+        <div className={classes.container}>
             {alerts.map(a => (
                 // Issue#61 think about only displaying the last alert
-                <div key={a.id} className={`alert alert${a.type} mb-2`}>
-                    {a.msg}
+                <div
+                    key={a.id}
+                    className={clsx(classes.alert, classes[a.type])}
+                >
+                    <Typography variant='h5'>{a.msg}</Typography>
                 </div>
             ))}
         </div>
