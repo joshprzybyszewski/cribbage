5c1a2c6c2bd98bc155b7bddf79a411ae978d5473
Connor Szczepaniak <cszczepaniak@gmail.com>
2021-03-18 19:43:08 -0500

    add comments for maps

--- client/src/app/containers/Game/types.ts
+++ client/src/app/containers/Game/types.ts
@@ -7,9 +7,11 @@ export interface ActionInputProps {
 export interface CreateGameResponse {
     id: number;
     players: User[];
+    // maps playerId -> peg color
     player_colors: {
         [key: string]: string;
     };
+    // maps playerId -> blocking reason
     blocking_players: {
         [key: string]: string;
     };
