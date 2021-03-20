5c410686740c7496d1bf8adfb8ca906d7648a3c8
Josh Przybyszewski <josh.prezby@gmail.com>
2021-03-20 17:30:28 -0500

    consistency?

--- client/src/app/containers/Suggestions/slice.ts
+++ client/src/app/containers/Suggestions/slice.ts
@@ -63,7 +63,7 @@ const suggestionsSlice = createSlice({
             const update = action.payload;
             const cardsAreEqual = (c1: Card, c2: Card) =>
                 c1.suit === c2.suit && c1.value === c2.value;
-            if (state.handCards.some((c) => cardsAreEqual(c, update.cur)) {
+            if (state.handCards.some((c) => cardsAreEqual(c, update.cur))) {
                 // do not update if the current hand has the "update to" card in it already
                 return {
                     ...state,
