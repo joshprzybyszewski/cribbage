2dc172fb69ee86d1b3f13f9dc0ae80a81eb2c049
Josh Przybyszewski <josh.prezby@gmail.com>
2021-03-23 07:22:32 -0500

    clean things up a little

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,28 +1,41 @@
+import * as ec2 from "@aws-cdk/aws-ec2";
+import * as ecs_patterns from "@aws-cdk/aws-ecs-patterns";
 import * as cdk from "@aws-cdk/core";
 import * as ecs from "@aws-cdk/aws-ecs";
-import * as ecsPatterns from "@aws-cdk/aws-ecs-patterns";
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const serverPort = 8080;
+    const serverPort = 80;
 
-    new ecsPatterns.ApplicationLoadBalancedFargateService(this, `${id}-ecs`, {
-      memoryLimitMiB: 1024,
-      cpu: 512,
+    const vpc = new ec2.Vpc(this, `${id}-vpc`, {
+      maxAzs: 3 // Default is all AZs in region
+    });
+
+    const cluster = new ecs.Cluster(this, `${id}-cluster`, {
+      vpc: vpc
+    });
+
+    new ecs_patterns.ApplicationLoadBalancedFargateService(this, `${id}-fargate`, {
+      cluster: cluster, // Required
+      memoryLimitMiB: 1024, // Default is 512
+      cpu: 512, // Default is 256
+      desiredCount: 1, // Default is 1
+      publicLoadBalancer: true, // Default is false
       taskImageOptions: {
-        image: ecs.ContainerImage.fromAsset("../", {
-          exclude: ["infrastructure/*"],
-        }),
-        // here's how we'd grab the image from docker hub:
-        // image: ecs.ContainerImage.fromRegistry(
-        //   "joshprzybyszewski/cribbage:latest"
-        // ),
+        // here's how we'd grab the image from dockerhub:
+        image: ecs.ContainerImage.fromRegistry(
+          "joshprzybyszewski/cribbage:latest" // TODO change this to use `master`, not `latest`.
+        ),
         containerPort: serverPort,
         environment: {
-          CRIBBAGE_DB: "memory",
+          CRIBBAGE_DB: "memory", // TODO swap this to "mysql"
           CRIBBAGE_RESTPORT: serverPort.toString(),
+          CRIBBAGE_DSN_HOST: "TODO_get_the_URL_to_the_rds",
+          CRIBBAGE_DSN_PARAMS: "parseTime=true&timeout=90s&writeTimeout=90s&readTimeout=90s&tls=skip-verify&maxAllowedPacket=1000000000&rejectReadOnly=true",
+          CRIBBAGE_DSN_USER: "TODO_figure_out_my_user",
+          CRIBBAGE_DSN_PASSWORD: "TODO_give_that_user_a_pw",
         },
       },
     });
