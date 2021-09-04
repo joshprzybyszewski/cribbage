8b1f73062ef4579529d320dc7cfacf81c9f78be5
Connor Szczepaniak <cszczepaniak@gmail.com>
2021-09-04 16:21:15 -0500

    try to avoid the linter; save old code

--- logic/scorer/scorer.go
+++ logic/scorer/scorer.go
@@ -53,7 +53,7 @@ func points(lead model.Card, hand []model.Card, isCrib bool) int {
 	allScoreTypes = allScoreTypes | st
 	totalPoints += pts
 
-	st, pts = scoreRunsAndPairs(vals)
+	st, pts = scoreRunsAndPairsV2(vals)
 	allScoreTypes = allScoreTypes | st
 	totalPoints += pts
 
@@ -206,7 +206,7 @@ func calculateTypeAndPoints(longest, mult uint8) (scoreType, int) {
 	return st, int(longest) * int(mult)
 }
 
-func scoreRunsAndPairs(values values) (scoreType, int) { //nolint:gocyclo
+func scoreRunsAndPairsV2(values values) (scoreType, int) {
 	var valuesToCounts valueToCount
 	for _, v := range values {
 		valuesToCounts[v]++
@@ -214,185 +214,187 @@ func scoreRunsAndPairs(values values) (scoreType, int) { //nolint:gocyclo
 	pairType, pairPts := scorePairs(values, valuesToCounts)
 	runType, runPts := scoreRuns(values, valuesToCounts)
 	return pairType | runType, pairPts + runPts
+}
 
-	// min := values[0]
-	// max := values[4]
+func scoreRunsAndPairs(values []int) (scoreType, int) { //nolint:gocyclo,go-staticcheck
+	min := values[0]
+	max := values[4]
 
-	// // check quad for all hands is the same
-	// if values[3] == min || values[1] == max {
-	// 	// this is a quad of the either form
-	// 	// Y Y Y Y|X (wlog cuz Y|X X X X)
-	// 	return quad, 12 /* 4 of a kind is worth 12 */
-	// }
+	// check quad for all hands is the same
+	if values[3] == min || values[1] == max {
+		// this is a quad of the either form
+		// Y Y Y Y|X (wlog cuz Y|X X X X)
+		return quad, 12 /* 4 of a kind is worth 12 */
+	}
 
-	// diffOf5Cards := max - min
+	diffOf5Cards := max - min
 
-	// if diffOf5Cards == 1 {
-	// 	// this hand is either
-	// 	// A A A|B B (wlog cuz A A|B B B)
-	// 	// A A A A|B (wlog cuz A|B B B B)
-	// 	// but we've already checked the quad
-	// 	// therefore this is a triple and a pair
-	// 	return triplet | onepair, 8 /* 6 for a triple, 2 for a pair */
-	// } else if diffOf5Cards == 2 {
-	// 	// check hands with triples
-	// 	if values[1] == values[3] {
-	// 		// we know that values[0] = values[1] - 1 and values[3] = values[4] + 1
-	// 		// because we already checked quads
-	// 		// A|B B B|C
-	// 		return tripleRunOfThree, 15 /* triple run of 3 */
-	// 	} else if values[2] == min {
-	// 		// A A A|B|C
-	// 		// A A A|C C
-	// 		if values[3] == max {
-	// 			// triple/pair combo
-	// 			return triplet | onepair, 8 /* 6 for a triple, 2 for a pair */
-	// 		}
-	// 		// triple run of 3 is worth 15
-	// 		// because 3 runs of three (worth 9) and a triple for 6
-	// 		return tripleRunOfThree, 15 /* triple run of 3 */
-	// 	} else if values[2] == max {
-	// 		// A|B|C C C
-	// 		// A A|C C C
-	// 		if values[1] == min {
-	// 			return triplet | onepair, 8 /* 6 for a triple, 2 for a pair */
-	// 		}
-	// 		return tripleRunOfThree, 15 /* triple run of 3 */
-	// 	}
+	if diffOf5Cards == 1 {
+		// this hand is either
+		// A A A|B B (wlog cuz A A|B B B)
+		// A A A A|B (wlog cuz A|B B B B)
+		// but we've already checked the quad
+		// therefore this is a triple and a pair
+		return triplet | onepair, 8 /* 6 for a triple, 2 for a pair */
+	} else if diffOf5Cards == 2 {
+		// check hands with triples
+		if values[1] == values[3] {
+			// we know that values[0] = values[1] - 1 and values[3] = values[4] + 1
+			// because we already checked quads
+			// A|B B B|C
+			return tripleRunOfThree, 15 /* triple run of 3 */
+		} else if values[2] == min {
+			// A A A|B|C
+			// A A A|C C
+			if values[3] == max {
+				// triple/pair combo
+				return triplet | onepair, 8 /* 6 for a triple, 2 for a pair */
+			}
+			// triple run of 3 is worth 15
+			// because 3 runs of three (worth 9) and a triple for 6
+			return tripleRunOfThree, 15 /* triple run of 3 */
+		} else if values[2] == max {
+			// A|B|C C C
+			// A A|C C C
+			if values[1] == min {
+				return triplet | onepair, 8 /* 6 for a triple, 2 for a pair */
+			}
+			return tripleRunOfThree, 15 /* triple run of 3 */
+		}
 
-	// 	// If the hand doesn't have a quad or a triple, then it must be a double-double run of three
-	// 	return doubleDoubleRunOfThree, 16 /* double double run of 3 */
-	// }
+		// If the hand doesn't have a quad or a triple, then it must be a double-double run of three
+		return doubleDoubleRunOfThree, 16 /* double double run of 3 */
+	}
 
-	// // checking triplets for remaining hands are all the same
-	// // look for (triple/pair, triple/odds)
-	// if values[1] == values[3] {
-	// 	// this is definitely a triple without a pair
-	// 	// A|B B B|D
-	// 	return triplet, 6 /* triplet */
-	// } else if values[2] == min {
-	// 	// A A A|B|D
-	// 	// A A A|D D
-	// 	if values[3] == max {
-	// 		// triple/pair combo
-	// 		return triplet | onepair, 8 /* 6 for a triple, 2 for a pair */
-	// 	}
-	// 	return triplet, 6 /* triplet */
-	// } else if values[2] == max {
-	// 	// A|B|D D D
-	// 	// A A|D D D
-	// 	if values[1] == min {
-	// 		return triplet | onepair, 8 /* 6 for a triple, 2 for a pair */
-	// 	}
-	// 	return triplet, 6 /* triplet */
-	// }
+	// checking triplets for remaining hands are all the same
+	// look for (triple/pair, triple/odds)
+	if values[1] == values[3] {
+		// this is definitely a triple without a pair
+		// A|B B B|D
+		return triplet, 6 /* triplet */
+	} else if values[2] == min {
+		// A A A|B|D
+		// A A A|D D
+		if values[3] == max {
+			// triple/pair combo
+			return triplet | onepair, 8 /* 6 for a triple, 2 for a pair */
+		}
+		return triplet, 6 /* triplet */
+	} else if values[2] == max {
+		// A|B|D D D
+		// A A|D D D
+		if values[1] == min {
+			return triplet | onepair, 8 /* 6 for a triple, 2 for a pair */
+		}
+		return triplet, 6 /* triplet */
+	}
 
-	// switch diffOf5Cards {
-	// case 3:
-	// 	// If we have a diff of 3, then we either have a two pair or a double run of 4
-	// 	// check double run of four: A A|B|C|D
-	// 	// check two-pair          : A A|B B|D
-	// 	numPairs := 0
-	// 	for i := 1; i < len(values); i++ {
-	// 		if values[i] == values[i-1] {
-	// 			numPairs++
-	// 		}
-	// 	}
+	switch diffOf5Cards {
+	case 3:
+		// If we have a diff of 3, then we either have a two pair or a double run of 4
+		// check double run of four: A A|B|C|D
+		// check two-pair          : A A|B B|D
+		numPairs := 0
+		for i := 1; i < len(values); i++ {
+			if values[i] == values[i-1] {
+				numPairs++
+			}
+		}
 
-	// 	if numPairs == 2 {
-	// 		// we found two pairs, which mean it cannot be a double run of 4
-	// 		return twopair, 4 /* two pair */
-	// 	}
+		if numPairs == 2 {
+			// we found two pairs, which mean it cannot be a double run of 4
+			return twopair, 4 /* two pair */
+		}
 
-	// 	return doubleRunOfFour, 10 /* double run of four */
+		return doubleRunOfFour, 10 /* double run of four */
 
-	// case 4:
-	// 	// So here's the options:
-	// 	// Run of 5
-	// 	// 1,2,3,4,5
-	// 	// double run of three
-	// 	// 1,2,x,3,5
-	// 	// two pair
-	// 	// 1,1,3,3,5
-	// 	// 1,3,3,5,5
-	// 	// pair + run of three
-	// 	// 1,2,3,5,5
-	// 	// 1,1,3,4,5
-	// 	// pair alone (regardless of fifteens)
-	// 	// 6,6,7,9,10
-	// 	// 6,7,9,10,10
-	// 	var numPairs, numIncs int
-	// 	for i := 0; i < len(values)-1; i++ {
-	// 		if iv, nv := values[i], values[i+1]; iv == nv {
-	// 			numPairs++
-	// 		} else if iv+1 == nv {
-	// 			numIncs++
-	// 		}
-	// 	}
-	// 	if numPairs == 2 {
-	// 		return twopair, 4 /* two pair */
-	// 	} else if numIncs == 4 {
-	// 		return run5, 5 /* run of 5 */
-	// 	} else if values[2] == max-2 && (values[3] == max || values[1] == min) {
-	// 		return run3 | onepair, 5 /* run of three and a pair*/
-	// 	} else if (values[2] == min+1 && values[3] != values[2]+1) ||
-	// 		(values[2] == max-1 && values[1] != values[2]-1) {
-	// 		return onepair, 2 /* a pair*/
-	// 	}
+	case 4:
+		// So here's the options:
+		// Run of 5
+		// 1,2,3,4,5
+		// double run of three
+		// 1,2,x,3,5
+		// two pair
+		// 1,1,3,3,5
+		// 1,3,3,5,5
+		// pair + run of three
+		// 1,2,3,5,5
+		// 1,1,3,4,5
+		// pair alone (regardless of fifteens)
+		// 6,6,7,9,10
+		// 6,7,9,10,10
+		var numPairs, numIncs int
+		for i := 0; i < len(values)-1; i++ {
+			if iv, nv := values[i], values[i+1]; iv == nv {
+				numPairs++
+			} else if iv+1 == nv {
+				numIncs++
+			}
+		}
+		if numPairs == 2 {
+			return twopair, 4 /* two pair */
+		} else if numIncs == 4 {
+			return run5, 5 /* run of 5 */
+		} else if values[2] == max-2 && (values[3] == max || values[1] == min) {
+			return run3 | onepair, 5 /* run of three and a pair*/
+		} else if (values[2] == min+1 && values[3] != values[2]+1) ||
+			(values[2] == max-1 && values[1] != values[2]-1) {
+			return onepair, 2 /* a pair*/
+		}
 
-	// 	return doubleRunOfThree, 8 /* double run of 3 */
-	// }
+		return doubleRunOfThree, 8 /* double run of 3 */
+	}
 
-	// // check run of 4 (and middle set run of 3)
-	// if (values[1]+1 == values[2]) && (values[2]+1 == values[3]) {
-	// 	if (min+1 == values[1]) || (values[3]+1 == max) {
-	// 		// there's no way all 5 can be, so we just check for 4
-	// 		return run4, 4
-	// 	}
-	// 	// since we were checking for 4, but we found the middle three to be a run,
-	// 	// then we can check the two ends for a double run
-	// 	if min == values[1] || values[3] == max {
-	// 		return doubleRunOfThree, 8 /* double run of 3 */
-	// 	}
-	// 	return run3, 3 /* run of 3 */
-	// }
+	// check run of 4 (and middle set run of 3)
+	if (values[1]+1 == values[2]) && (values[2]+1 == values[3]) {
+		if (min+1 == values[1]) || (values[3]+1 == max) {
+			// there's no way all 5 can be, so we just check for 4
+			return run4, 4
+		}
+		// since we were checking for 4, but we found the middle three to be a run,
+		// then we can check the two ends for a double run
+		if min == values[1] || values[3] == max {
+			return doubleRunOfThree, 8 /* double run of 3 */
+		}
+		return run3, 3 /* run of 3 */
+	}
 
-	// numPairs := 0
-	// for i := 1; i < len(values); i++ {
-	// 	if values[i] == values[i-1] {
-	// 		numPairs++
-	// 	}
-	// }
-	// if numPairs == 2 {
-	// 	return twopair, 4 /* two points per pair*/
-	// }
+	numPairs := 0
+	for i := 1; i < len(values); i++ {
+		if values[i] == values[i-1] {
+			numPairs++
+		}
+	}
+	if numPairs == 2 {
+		return twopair, 4 /* two points per pair*/
+	}
 
-	// if values[3]-min == 2 {
-	// 	// either two pair or a double run of three
-	// 	// but we already checked two pair
-	// 	return doubleRunOfThree, 8 /* double run of 3 */
-	// } else if max-values[1] == 2 {
-	// 	// either two pair or a double run of three
-	// 	// but we already checked two pair
-	// 	return doubleRunOfThree, 8 /* double run of 3 */
-	// }
+	if values[3]-min == 2 {
+		// either two pair or a double run of three
+		// but we already checked two pair
+		return doubleRunOfThree, 8 /* double run of 3 */
+	} else if max-values[1] == 2 {
+		// either two pair or a double run of three
+		// but we already checked two pair
+		return doubleRunOfThree, 8 /* double run of 3 */
+	}
 
-	// var st scoreType
-	// pts := 0
+	var st scoreType
+	pts := 0
 
-	// if ((min+1 == values[1]) && (values[1]+1 == values[2])) ||
-	// 	((values[2]+1 == values[3]) && (values[3]+1 == max)) {
-	// 	st = st | run3 /* run of 3 (and maybe a pair) */
-	// 	pts += 3
-	// }
+	if ((min+1 == values[1]) && (values[1]+1 == values[2])) ||
+		((values[2]+1 == values[3]) && (values[3]+1 == max)) {
+		st = st | run3 /* run of 3 (and maybe a pair) */
+		pts += 3
+	}
 
-	// if numPairs == 1 {
-	// 	// we already checked for two above
-	// 	st = st | onepair /* one pair */
-	// 	pts += 2
-	// }
+	if numPairs == 1 {
+		// we already checked for two above
+		st = st | onepair /* one pair */
+		pts += 2
+	}
 
-	// return st, pts
+	return st, pts
 }
 
 func scoreFlushesAndNobs(lead model.Card, hand []model.Card, isCrib bool) (scoreType, int) {
