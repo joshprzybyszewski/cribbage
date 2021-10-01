044263aa57df5ede8ba32b0f00a52f1a58882a05
Josh Przybyszewski <josh.prezby@gmail.com>
2021-10-01 18:22:47 -0500

    make it work?

--- server/persistence/dynamo/dynamo_service_game.go
+++ server/persistence/dynamo/dynamo_service_game.go
@@ -22,23 +22,15 @@ const (
 	gameServiceSortKeyPrefix string = `game`
 )
 
-type gameList struct {
-	GameID model.GameID `bson:"gameID"`
-	Games  []model.Game `bson:"games,omitempty"`
-}
-
-func (gl *gameList) add(i int, g model.Game) {
-	// TODO
-	for i >= len(gl.Games) {
-		gl.Games = append(gl.Games, model.Game{})
-	}
-	gl.Games[i] = g
+type gameAtAction struct {
+	Game        model.Game
+	ActionIndex int
+	Overwrite   bool
 }
 
 type getGameOptions struct {
-	latest  bool
-	all     bool
-	actions map[int]struct{}
+	latest      bool
+	actionIndex uint
 }
 
 var _ persistence.GameService = (*gameService)(nil)
@@ -61,63 +53,35 @@ func getGameService(
 }
 
 func (gs *gameService) Get(id model.GameID) (model.Game, error) {
-	return gs.getSingleGame(id, getGameOptions{
+	return gs.getGame(id, getGameOptions{
 		latest: true,
 	})
 }
 
 func (gs *gameService) GetAt(id model.GameID, numActions uint) (model.Game, error) {
-	return gs.getSingleGame(id, getGameOptions{
-		actions: map[int]struct{}{int(numActions): {}},
+	return gs.getGame(id, getGameOptions{
+		actionIndex: numActions,
 	})
 }
 
-func (gs *gameService) getSingleGame(
+func (gs *gameService) getGame(
 	id model.GameID,
 	opts getGameOptions,
-) (model.Game, error) {
-
-	games, err := gs.getGameStates(id, opts)
-	if err != nil {
-		return model.Game{}, err
-	}
-	if len(games) != 1 {
-		return model.Game{}, errors.New(`action doesn't exist`)
-	}
-	return games[0], nil
-}
-
-func (gs *gameService) getGameStates(id model.GameID, opts getGameOptions) ([]model.Game, error) { // nolint:gocyclo
-	gl := gameList{}
-
-	// I want to minimize the number of dynamo tables I use:
-	// "You should maintain as few tables as possible in a DynamoDB application."
-	// -https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/bp-general-nosql-design.html
-	// input := &dynamodb.BatchGetItemInput{
-	// 	RequestItems: map[string]*dynamodb.KeysAndAttributes{
-	// 		dbName: {
-	// 			Keys: []map[string]*dynamodb.AttributeValue{{
-	// 				partitionKey: &dynamodb.AttributeValue{
-	// 					S: aws.String(string(id)),
-	// 				},
-	// 				// TODO I don't remember right now how to get based on partition/sort key
-	// 			}},
-	// 			// TODO if only latest, then we can use a projexp to filter down
-	// 			ProjectionExpression: aws.String("max(numGameActions)"),
-	// 		},
-	// 	},
-	// }
-
-	// result, err := gs.svc.BatchGetItem(gs.ctx, input)
-	// if err != nil {
-
+) (model.Game, error) { // nolint:gocyclo
 	tableName := dbName
 	pkName := `:gID`
 	pk := strconv.Itoa(int(id))
 	skName := `:sk`
-	sk := gameServiceSortKeyPrefix
+	sk := gameServiceSortKeyPrefix + `@`
+	if !opts.latest {
+		sk = getSpecForGameActionIndex(int(opts.actionIndex))
+	}
 	keyCondExpr := fmt.Sprintf("DDBid = %s and begins_with(spec, %s)", pkName, skName)
-	qo, err := gs.svc.Query(gs.ctx, &dynamodb.QueryInput{
+
+	sif := false
+
+	qi := &dynamodb.QueryInput{
+		ScanIndexForward:       &sif,
 		TableName:              &tableName,
 		KeyConditionExpression: &keyCondExpr,
 		ExpressionAttributeValues: map[string]types.AttributeValue{
@@ -128,54 +92,42 @@ func (gs *gameService) getGameStates(id model.GameID, opts getGameOptions) ([]mo
 				Value: sk,
 			},
 		},
-	})
+	}
+	qo, err := gs.svc.Query(gs.ctx, qi)
 	if err != nil {
 		fmt.Println(err)
+		return model.Game{}, err
 	}
-	fmt.Printf("query for games: %+v\n", qo)
-
-	if opts.actions == nil {
-		opts.actions = make(map[int]struct{})
+	if len(qo.Items) == 0 {
+		return model.Game{}, errors.New(`unexpected number of items returned`)
 	}
 
-	var highestI int
-	var highestG model.Game
-
-	for _, item := range qo.Items {
+	item := qo.Items[0]
+	if !opts.latest {
+		// make sure that the index we got back matches the one we requested
 		spec, ok := item[`spec`].(*types.AttributeValueMemberS)
 		if !ok {
-			return nil, persistence.ErrGameActionDecode
+			return model.Game{}, persistence.ErrGameActionDecode
 		}
 		i, err := getGameActionIndexFromSpec(spec.Value)
 		if err != nil {
-			return nil, err
+			return model.Game{}, err
 		}
-
-		gb, ok := item[`gameBytes`].(*types.AttributeValueMemberB)
-		if !ok {
-			return nil, persistence.ErrGameActionDecode
+		if i != int(opts.actionIndex) {
+			return model.Game{}, errors.New(`retrieved unexpected game action index`)
 		}
-		g, err := jsonutils.UnmarshalGame(gb.Value)
-		if err != nil {
-			return nil, err
-		}
-		if i > highestI {
-			highestI = i
-			highestG = g
-		}
-
-		if _, ok := opts.actions[i]; !ok && !opts.all {
-			continue
-		}
-
-		gl.add(i, g)
 	}
 
-	if opts.latest {
-		gl.add(highestI, highestG)
+	gb, ok := item[`gameBytes`].(*types.AttributeValueMemberB)
+	if !ok {
+		return model.Game{}, persistence.ErrGameActionDecode
+	}
+	g, err := jsonutils.UnmarshalGame(gb.Value)
+	if err != nil {
+		return model.Game{}, err
 	}
 
-	return gl.Games, nil
+	return g, nil
 }
 
 func (gs *gameService) UpdatePlayerColor(gID model.GameID, pID model.PlayerID, color model.PlayerColor) error {
@@ -193,26 +145,16 @@ func (gs *gameService) UpdatePlayerColor(gID model.GameID, pID model.PlayerID, c
 		return nil
 	}
 
-	games, err := gs.getGameStates(gID, getGameOptions{
-		all: true,
+	if g.PlayerColors == nil {
+		g.PlayerColors = make(map[model.PlayerID]model.PlayerColor, 1)
+	}
+	g.PlayerColors[pID] = color
+
+	return gs.saveGame(gameAtAction{
+		Game:        g,
+		ActionIndex: len(g.Actions) - 1,
+		Overwrite:   true,
 	})
-	if err != nil {
-		return err
-	}
-
-	recentGame := games[len(games)-1]
-	if recentGame.PlayerColors == nil {
-		recentGame.PlayerColors = make(map[model.PlayerID]model.PlayerColor, 1)
-	}
-	recentGame.PlayerColors[pID] = color
-
-	games[len(games)-1] = recentGame
-	newGameList := gameList{
-		GameID: gID,
-		Games:  games,
-	}
-
-	return gs.saveGameList(newGameList)
 }
 
 func (gs *gameService) Begin(g model.Game) error {
@@ -221,19 +163,12 @@ func (gs *gameService) Begin(g model.Game) error {
 
 func (gs *gameService) Save(g model.Game) error {
 
-	games, err := gs.getGameStates(g.ID, getGameOptions{
-		all: true,
+	// TODO replace this with a getGameCurrentActionIndex
+	sg, err := gs.getGame(g.ID, getGameOptions{
+		latest: true,
 	})
 	if err != nil {
 		return err
-	}
-
-	saved := gameList{
-		GameID: g.ID,
-		Games:  games,
-	}
-	if saved.GameID != g.ID {
-		return errors.New(`bad save somewhere`)
 	}
 
 	// TODO does this still belong?
@@ -242,13 +177,18 @@ func (gs *gameService) Save(g model.Game) error {
 	// 	return err
 	// }
 
-	saved.Games = append(saved.Games, g)
-
-	return gs.saveGameList(saved)
+	return gs.saveGame(gameAtAction{
+		Game:        g,
+		ActionIndex: len(sg.Actions),
+		Overwrite:   true,
+	})
 }
 
 func getSpecForGameActionIndex(i int) string {
-	return gameServiceSortKeyPrefix + `@` + strconv.Itoa(i)
+	// Since we print out leading zeros to nine places, we could
+	// have issues if our cribbage games ever take more than
+	// 999,999,999 actions
+	return gameServiceSortKeyPrefix + `@` + fmt.Sprintf("%09d", i)
 }
 
 func getGameActionIndexFromSpec(s string) (int, error) {
@@ -256,39 +196,42 @@ func getGameActionIndexFromSpec(s string) (int, error) {
 	return strconv.Atoi(s)
 }
 
-func (gs *gameService) saveGameList(saved gameList) error {
-	// craft an id that uses len(saved.Games)
-
-	obj, err := json.Marshal(saved.Games[len(saved.Games)-1])
+func (gs *gameService) saveGame(gaa gameAtAction) error {
+	obj, err := json.Marshal(gaa.Game)
 	if err != nil {
 		return err
 	}
 
 	data := map[string]types.AttributeValue{
 		`DDBid`: &types.AttributeValueMemberS{
-			Value: strconv.Itoa(int(saved.GameID)),
+			Value: strconv.Itoa(int(gaa.Game.ID)),
 		},
 		`spec`: &types.AttributeValueMemberS{
-			Value: gameServiceSortKeyPrefix + `@` + strconv.Itoa(len(saved.Games)),
+			Value: getSpecForGameActionIndex(gaa.ActionIndex),
 		},
 		`gameBytes`: &types.AttributeValueMemberB{
 			Value: obj,
 		},
 	}
 
-	// Use a conditional expression to only write items if this
-	// <HASH:RANGE> tuple doesn't already exist.
-	// See: https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Expressions.ConditionExpressions.html
-	// and https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Expressions.OperatorsAndFunctions.html
-	condExpr := `attribute_not_exists(DDBid) AND attribute_not_exists(spec)`
-
 	fmt.Printf("playerService.Create data = %+v\n", data)
 
-	_, err = gs.svc.PutItem(gs.ctx, &dynamodb.PutItemInput{
-		TableName:           aws.String(dbName),
-		Item:                data,
-		ConditionExpression: &condExpr,
-	})
+	pii := &dynamodb.PutItemInput{
+		TableName: aws.String(dbName),
+		Item:      data,
+	}
+
+	if !gaa.Overwrite {
+		// Use a conditional expression to only write items if this
+		// <HASH:RANGE> tuple doesn't already exist.
+		// See: https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Expressions.ConditionExpressions.html
+		// and https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Expressions.OperatorsAndFunctions.html
+		condExpr := `attribute_not_exists(DDBid) AND attribute_not_exists(spec)`
+
+		pii.ConditionExpression = &condExpr
+	}
+
+	_, err = gs.svc.PutItem(gs.ctx, pii)
 	if err != nil {
 		switch err.(type) {
 		case *types.ConditionalCheckFailedException:
@@ -296,26 +239,7 @@ func (gs *gameService) saveGameList(saved gameList) error {
 		}
 		return err
 	}
+	// TODO: if this did NOT overwrite something, then that should be an error
 
 	return nil
-	/*
-		filter := bsonGameIDFilter(saved.GameID)
-		return mongo.WithSession(gs.ctx, gs.session, func(sc mongo.SessionContext) error {
-			ur, err := gs.col.ReplaceOne(sc, filter, saved)
-			if err != nil {
-				return err
-			}
-
-			switch {
-			case ur.ModifiedCount > 1:
-				return errors.New(`modified too many games`)
-			case ur.MatchedCount > 1:
-				return errors.New(`matched more than one game entry`)
-			case ur.UpsertedCount > 1:
-				return errors.New(`replaced more than one game`)
-			}
-
-			return nil
-		})
-	*/
 }
