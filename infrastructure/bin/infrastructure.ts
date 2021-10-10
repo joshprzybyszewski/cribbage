1c072bce34eeb751416ddc9a2bde461866560581
Josh Przybyszewski <josh.prezby@gmail.com>
2021-10-09 23:07:39 -0500

    I have no idea what i'm doing

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -2,35 +2,41 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { VpcStack } from '../lib/vpc-stack';
-import { FargateAppStack } from '../lib/fargate-app-stack';
-import { RDSStack } from '../lib/rds-stack';
+import { DynamoDBStack } from '../lib/dynamodb-stack';
+import { LambdaStack } from '../lib/lambda-stack';
+// import { FargateAppStack } from '../lib/fargate-app-stack';
+// import { RDSStack } from '../lib/rds-stack';
 import { Port } from '@aws-cdk/aws-ec2';
 import { DNSStack } from '../lib/dns-stack';
 import { Environment } from '@aws-cdk/core';
 
-const env: Environment = { account: process.env['AWS_ACCOUNT'], region: process.env['AWS_REGION'] };
+const env: Environment = {
+    account: process.env['AWS_ACCOUNT'],
+    region: process.env['AWS_REGION'],
+};
 
 const app = new cdk.App();
 
-console.log('building vpc...');
-const vpcStackEntity = new VpcStack(app, 'cribbage-vpc', { env });
-console.log('building vpc...Done!');
+// console.log('building vpc...');
+// const vpcStackEntity = new VpcStack(app, 'cribbage-vpc', { env });
+// console.log('building vpc...Done!');
 
 console.log('building dynamodb table...');
-const rdsStack = new DynamoDBStack(app, 'cribbage-dynamodb', {
-    vpc: vpcStackEntity.vpc,
-    rdsIngressPort: vpcStackEntity.rdsPort, // TODO
+const dynamoStack = new DynamoDBStack(app, 'cribbage-dynamodb', {
     env,
 });
 console.log('building dynamodb table...Done!');
 
 console.log('building lambdas...');
-const fargateStack = new LambdaStack(app, 'cribbage-lambda', {
-    vpc: vpcStackEntity.vpc,
-    dbSecretArn: rdsStack.mySQLRDSInstance.secret?.secretArn, // TODO
+const lambdaStack = new LambdaStack(app, 'cribbage-lambda', {
+    table: dynamoStack.table,
     env,
 });
 console.log('building lambdas...Done!');
+
+console.log('Granting RW access to dynamo from lambdas...');
+dynamoStack.table.grantReadWriteData(lambdaStack.function);
+console.log('Granting RW access to dynamo from lambdas...Done!');
 
 // console.log('building rds...');
 // const rdsStack = new RDSStack(app, 'cribbage-rds', {
@@ -48,15 +54,15 @@ console.log('building lambdas...Done!');
 // });
 // console.log('building fargate app...Done!');
 
-console.log('building dns...');
-new DNSStack(app, 'cribbage-dns', {
-    loadBalancer: fargateStack.albFargateService.loadBalancer,
-    env,
-});
-console.log('building dns...Done!');
+// console.log('building dns...');
+// new DNSStack(app, 'cribbage-dns', {
+//     loadBalancer: fargateStack.albFargateService.loadBalancer,
+//     env,
+// });
+// console.log('building dns...Done!');
 
 // I read the following doc which recommended peering two constructs 
 // using connections in python: https://docs.aws.amazon.com/cdk/api/latest/python/aws_cdk.aws_ec2/SecurityGroup.html
-console.log('Allowing connections between constructs...');
-fargateStack.albFargateService.service.connections.allowTo(rdsStack.mySQLRDSInstance, Port.tcp(vpcStackEntity.rdsPort));
-console.log('Allowing connections between constructs...Done!');
+// console.log('Allowing connections between constructs...');
+// fargateStack.albFargateService.service.connections.allowTo(rdsStack.mySQLRDSInstance, Port.tcp(vpcStackEntity.rdsPort));
+// console.log('Allowing connections between constructs...Done!');
--- infrastructure/lib/lambda-stack.ts
+++ infrastructure/lib/lambda-stack.ts
@@ -1,57 +1,37 @@
-import * as ec2 from '@aws-cdk/aws-ec2';
 import * as ecs_patterns from '@aws-cdk/aws-ecs-patterns';
 import * as cdk from '@aws-cdk/core';
-import * as ecs from '@aws-cdk/aws-ecs';
-import { Secret } from '@aws-cdk/aws-secretsmanager';
-import { ARecord, HostedZone, RecordTarget } from '@aws-cdk/aws-route53';
-import { LoadBalancerTarget } from '@aws-cdk/aws-route53-targets';
+import {Code, Function, Runtime}  from '@aws-cdk/aws-lambda';
+import {  Table } from '@aws-cdk/aws-dynamodb';
+import * as assets from '@aws-cdk/aws-s3-assets';
+import * as path from 'path';
 
 export interface LambdaStackProps extends cdk.StackProps {
-    vpc: ec2.Vpc;
-    dbSecretArn: string | undefined;
+    readonly table: Table;
 }
 
 export class LambdaStack extends cdk.Stack {
     readonly albFargateService: ecs_patterns.ApplicationLoadBalancedFargateService;
+    readonly function: Function;
 
     constructor(scope: cdk.Construct, id: string, props: LambdaStackProps) {
         super(scope, id, props);
 
-        const dbUserSecret = Secret.fromSecretCompleteArn(this, `cribbageRDSSecret`, props.dbSecretArn ?? 'badarn');
+        const asset = new assets.Asset(this, 'SampleAsset', {
+            path: path.join(__dirname, '../cribbage-lambda.zip'),
+          });
 
-        var dsnHost = dbUserSecret.secretValueFromJson('dbname').toString();
-        var dbType = dsnHost.length > 0 ? 'mysql' : 'memory';
-
-        const serverPort = 80;
-
-        const vpc = props.vpc;
-
-        const cluster = new ecs.Cluster(this, `${id}-cluster`, {
-            vpc: vpc,
-        });
-
-        this.albFargateService = new ecs_patterns.ApplicationLoadBalancedFargateService(this, `${id}-fargate`, {
-            cluster: cluster, // Required
-            memoryLimitMiB: 1024, // Default is 512
-            cpu: 512, // Default is 256
-            desiredCount: 1, // Default is 1
-            publicLoadBalancer: true, // Default is false
-            taskImageOptions: {
-                image: ecs.ContainerImage.fromRegistry('joshprzybyszewski/cribbage:master'),
-                containerPort: serverPort,
-                environment: {
-                    CRIBBAGE_RESTPORT: serverPort.toString(),
-                    CRIBBAGE_DB: dbType,
-                    CRIBBAGE_DSN_HOST: dbUserSecret.secretValueFromJson('host').toString(),
-                    CRIBBAGE_DSN_USER: dbUserSecret.secretValueFromJson('username').toString(),
-                    CRIBBAGE_DSN_PASSWORD: dbUserSecret.secretValueFromJson('password').toString(),
-                    CRIBBAGE_MYSQL_DB: dbUserSecret.secretValueFromJson('dbname').toString(),
-                    CRIBBAGE_MYSQL_CREATE_TABLES: 'false', // mysql_create_tables may or may not be a good thing...
-                    CRIBBAGE_DSN_PARAMS:
-                        'parseTime=true&timeout=90s&writeTimeout=90s&readTimeout=90s&tls=skip-verify&maxAllowedPacket=1000000000&rejectReadOnly=true',
-                    deploy: 'prod',
-                },
+        this.function = new Function(this, 'cribbage-lambda-id', {
+            runtime: Runtime.GO_1_X,
+            // entry: path.join(__dirname, `../cribbage-lambda.zip`),
+            code: Code.fromAsset('../cribbage-lambda.zip'),
+            handler: 'cribbage-lambda',
+            timeout: cdk.Duration.seconds(15),
+            environment: {
+                'CRIBBAGE_DB': 'dynamodb',
+                'CRIBBAGE_LAMBDA': 'true',
+                'TABLE_NAME': props.table.tableName,
             },
-        });
+        })
+
     }
 }
